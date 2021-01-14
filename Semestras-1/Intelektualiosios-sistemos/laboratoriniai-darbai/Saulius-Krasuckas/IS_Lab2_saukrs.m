24c57227506374fe9a9c6b54417467a0e1e32b1c
saulius-krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-01-14 20:45:45 +0200

    IS_Lab2_saukrs.m: Ruošiu pirmą iteraciją kišti į ciklą

--- Semestras-1/Intelektualiosios-sistemos/laboratoriniai-darbai/Saulius-Krasuckas/IS_Lab2_saukrs.m
+++ Semestras-1/Intelektualiosios-sistemos/laboratoriniai-darbai/Saulius-Krasuckas/IS_Lab2_saukrs.m
@@ -25,27 +25,26 @@ w2_41_n0 = rand(1);
 b2_1_n0  = rand(1);
 
 % Skaičiuoju tinklo atsaką,
-% visų paslėptųjų neuronų įėjimai:
-v1 = x(1) * w1_11_n0 + b1_1_n0;
-v2 = x(1) * w1_21_n0 + b1_2_n0;
-v3 = x(1) * w1_31_n0 + b1_3_n0;
-v4 = x(1) * w1_41_n0 + b1_4_n0;
-
-% ... jų išėjimai:
-y1 = tanh(v1);
-y2 = tanh(v2);
-y3 = tanh(v3);
-y4 = tanh(v4);
-
-% iš. sl. neurono išėjimas:
-y = y1 * w2_11_n0 + ...
-    y2 * w2_21_n0 + ...
-    y3 * w2_31_n0 + ...
-    y4 * w2_41_n0 + ...
-         b2_1_n0;
 
 %for i=1:length(x)
-%    y1 = tanh(v1(i));
+    % visų paslėptųjų neuronų įėjimai:
+    v1 = x(i) * w1_11_n0 + b1_1_n0;
+    v2 = x(i) * w1_21_n0 + b1_2_n0;
+    v3 = x(i) * w1_31_n0 + b1_3_n0;
+    v4 = x(i) * w1_41_n0 + b1_4_n0;
+
+    % ... jų išėjimai:
+    y1 = tanh(v1);
+    y2 = tanh(v2);
+    y3 = tanh(v3);
+    y4 = tanh(v4);
+
+    % iš. sl. neurono išėjimas:
+    y(i) =      b2_1_n0  + ...
+           y1 * w2_11_n0 + ...
+           y2 * w2_21_n0 + ...
+           y3 * w2_31_n0 + ...
+           y4 * w2_41_n0;
 %end
 
 fprintf("Pabaiga.\n");
