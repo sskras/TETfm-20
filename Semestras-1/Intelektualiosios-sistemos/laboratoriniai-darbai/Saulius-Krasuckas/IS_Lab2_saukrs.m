4aec37929368a0a26134588d58d9ffb78ebce8ae
saulius-krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-01-14 20:16:43 +0200

    IS_Lab2_saukrs.m: Visi tarpiniai rezultatai pagyvens be masyvo, tikrai

--- Semestras-1/Intelektualiosios-sistemos/laboratoriniai-darbai/Saulius-Krasuckas/IS_Lab2_saukrs.m
+++ Semestras-1/Intelektualiosios-sistemos/laboratoriniai-darbai/Saulius-Krasuckas/IS_Lab2_saukrs.m
@@ -18,14 +18,23 @@ w1_21_n0 = rand(1);  b1_2_n0  = rand(1);
 w1_31_n0 = rand(1);  b1_3_n0  = rand(1);
 w1_41_n0 = rand(1);  b1_4_n0  = rand(1);
 
+w2_11_n0 = rand(1);
+w2_21_n0 = rand(1);
+w2_31_n0 = rand(1);
+w2_41_n0 = rand(1);
+b2_1_n0  = rand(1);
+
 % Skaičiuoju tinklo atsaką,
 % visų paslėptųjų neuronų įėjimai:
 v1 = x(1) * w1_11_n0 + b1_1_n0;
-v2(1) = x(1) * w1_21_n0 + b1_2_n0;
-v3(1) = x(1) * w1_31_n0 + b1_3_n0;
-v4(1) = x(1) * w1_41_n0 + b1_4_n0;
+v2 = x(1) * w1_21_n0 + b1_2_n0;
+v3 = x(1) * w1_31_n0 + b1_3_n0;
+v4 = x(1) * w1_41_n0 + b1_4_n0;
 
 y1 = tanh(v1);
+y2 = tanh(v2);
+y3 = tanh(v3);
+y4 = tanh(v4);
 
 %for i=1:length(x)
 %    y1 = tanh(v1(i));
