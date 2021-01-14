1b932d2b262faba6b6993a3c7f7e5e75b842169b
saulius-krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-01-14 18:41:55 +0200

    IS_Lab2_saukrs.m: smulku

--- Semestras-1/Intelektualiosios-sistemos/laboratoriniai-darbai/Saulius-Krasuckas/IS_Lab2_saukrs.m
+++ Semestras-1/Intelektualiosios-sistemos/laboratoriniai-darbai/Saulius-Krasuckas/IS_Lab2_saukrs.m
@@ -2,12 +2,12 @@ clc;
 
 % Aproksimavimo intervalas:
 fprintf("x:\n")
-x = 0.1:1/22:1
+x = 0.1:1/22:1;
 disp(x);
 
 % Aproksimuojamoji funkcija (tikrosios vertės):
 fprintf("d:\n")
-d = 5*sin(8*x) + 4*cos(16*x + pi/3)
+d = 5*sin(8*x) + 4*cos(16*x + pi/3);
 disp(d);
 
 fprintf("Pabaiga.\n")
