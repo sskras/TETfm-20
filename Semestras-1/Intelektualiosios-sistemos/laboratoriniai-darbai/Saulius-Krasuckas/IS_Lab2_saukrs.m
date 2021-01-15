099f3ba8ede81f88f7f0a6295c6dd3b4ca9b183a
saulius-krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-01-15 03:02:01 +0200

    IS_Lab2_saukrs.m: Pridedu du neuronus į paslėptąjį sl.

--- Semestras-1/Intelektualiosios-sistemos/laboratoriniai-darbai/Saulius-Krasuckas/IS_Lab2_saukrs.m
+++ Semestras-1/Intelektualiosios-sistemos/laboratoriniai-darbai/Saulius-Krasuckas/IS_Lab2_saukrs.m
@@ -20,11 +20,15 @@ w1_11_n0 = rand(1);  b1_1_n0  = rand(1);
 w1_21_n0 = rand(1);  b1_2_n0  = rand(1);
 w1_31_n0 = rand(1);  b1_3_n0  = rand(1);
 w1_41_n0 = rand(1);  b1_4_n0  = rand(1);
+w1_51_n0 = rand(1);  b1_5_n0  = rand(1);
+w1_61_n0 = rand(1);  b1_6_n0  = rand(1);
 
 w2_11_n0 = rand(1);
 w2_12_n0 = rand(1);
 w2_13_n0 = rand(1);
 w2_14_n0 = rand(1);
+w2_15_n0 = rand(1);
+w2_16_n0 = rand(1);
 b2_1_n0  = rand(1);
 
 % Skaičiuoju tinklo atsaką,
@@ -38,18 +42,24 @@ for i=1:length(x)
     v2 = x(i) * w1_21_n0 + b1_2_n0;
     v3 = x(i) * w1_31_n0 + b1_3_n0;
     v4 = x(i) * w1_41_n0 + b1_4_n0;
+    v5 = x(i) * w1_51_n0 + b1_5_n0;
+    v6 = x(i) * w1_61_n0 + b1_6_n0;
 
     % ... jų išėjimai:
     y1 = tanh(v1);
     y2 = tanh(v2);
     y3 = tanh(v3);
     y4 = tanh(v4);
+    y5 = tanh(v5);
+    y6 = tanh(v6);
 
     % iš. sl. neurono išėjimas:
     y(i) = y1 * w2_11_n0 + ...
            y2 * w2_12_n0 + ...
            y3 * w2_13_n0 + ...
            y4 * w2_14_n0 + ...
+           y5 * w2_15_n0 + ...
+           y6 * w2_16_n0 + ...
                 b2_1_n0;
 
     % iteracijos klaida:
@@ -69,6 +79,8 @@ for i=1:length(x)
     w2_12_n1 = w2_12_n0 + n * delta2 * y2;
     w2_13_n1 = w2_13_n0 + n * delta2 * y3;
     w2_14_n1 = w2_14_n0 + n * delta2 * y4;
+    w2_15_n1 = w2_15_n0 + n * delta2 * y5;
+    w2_16_n1 = w2_16_n0 + n * delta2 * y6;
     b2_1_n1  = b2_1_n0  + n * delta2;
 
     % paslėptojo sl. gradientai:
@@ -77,23 +89,31 @@ for i=1:length(x)
     delta1_2 = y(i) * (1 - y(i)) * delta2 * w2_12_n0;
     delta1_3 = y(i) * (1 - y(i)) * delta2 * w2_13_n0;
     delta1_4 = y(i) * (1 - y(i)) * delta2 * w2_14_n0;
+    delta1_5 = y(i) * (1 - y(i)) * delta2 * w2_15_n0;
+    delta1_6 = y(i) * (1 - y(i)) * delta2 * w2_16_n0;
 
     % paslėptojo sl. koeficientai (tikslinimas):
     w1_11_n0 = w1_11_n0 + n * delta1_1 * x(i);
     w1_21_n0 = w1_21_n0 + n * delta1_2 * x(i);
     w1_31_n0 = w1_31_n0 + n * delta1_3 * x(i);
     w1_41_n0 = w1_41_n0 + n * delta1_4 * x(i);
+    w1_51_n0 = w1_51_n0 + n * delta1_5 * x(i);
+    w1_61_n0 = w1_61_n0 + n * delta1_6 * x(i);
 
     b1_1_n0  = b1_1_n0  + n * delta1_1;
     b1_2_n0  = b1_2_n0  + n * delta1_2;
     b1_3_n0  = b1_3_n0  + n * delta1_3;
     b1_4_n0  = b1_4_n0  + n * delta1_4;
+    b1_5_n0  = b1_5_n0  + n * delta1_5;
+    b1_6_n0  = b1_6_n0  + n * delta1_6;
 
     % atnaujintus koeficientus įrašome į senųjų vietą (jau nebeverta saugoti):
     w2_11_n0 = w2_11_n1;
     w2_12_n0 = w2_12_n1;
     w2_13_n0 = w2_13_n1;
     w2_14_n0 = w2_14_n1;
+    w2_15_n0 = w2_15_n1;
+    w2_16_n0 = w2_16_n1;
     b2_1_n0  = b2_1_n1;
 
     %% galimai šito nereikės, kadangi gradientų skaičiavimui reikalingos
@@ -107,27 +127,37 @@ fprintf("e:"); disp(e);
 
 % patikrinkime perceptrono veiksnumą:
 
-x(i) = 0.7612; % y turėtų būti 2.198:
+x(i) = 0.7612; % tada y turėtų būti:
+d(i) = 2.198;
 
     v1 = x(i) * w1_11_n0 + b1_1_n0;
     v2 = x(i) * w1_21_n0 + b1_2_n0;
     v3 = x(i) * w1_31_n0 + b1_3_n0;
     v4 = x(i) * w1_41_n0 + b1_4_n0;
+    v5 = x(i) * w1_51_n0 + b1_5_n0;
+    v6 = x(i) * w1_61_n0 + b1_6_n0;
 
     % ... jų išėjimai:
     y1 = tanh(v1);
     y2 = tanh(v2);
     y3 = tanh(v3);
     y4 = tanh(v4);
+    y5 = tanh(v5);
+    y6 = tanh(v6);
 
     % iš. sl. neurono išėjimas:
     y(i) = y1 * w2_11_n0 + ...
            y2 * w2_12_n0 + ...
            y3 * w2_13_n0 + ...
            y4 * w2_14_n0 + ...
+           y5 * w2_15_n0 + ...
+           y6 * w2_16_n0 + ...
                 b2_1_n0;
 
-fprintf("x = %f, aproksimuotas y = %f.\n", x(i), y(i));
+    % iteracijos klaida:
+    e(i) = d(i) - y(i);
+
+fprintf("x = %f, d = %f, aproksimuotas y = %f, paklaida e = %f.\n", x(i), d(i), y(i), e(i));
 % kai x=0.7612, gauname y=1.212585 (vietoj 2.198)
 
 fprintf("Pabaiga.\n");
