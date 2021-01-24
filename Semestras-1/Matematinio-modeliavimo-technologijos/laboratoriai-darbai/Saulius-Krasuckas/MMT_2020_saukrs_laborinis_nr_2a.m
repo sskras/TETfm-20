46832de78683cf79eda74f9c0c196777e8b58436
saulius-krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-01-24 18:18:49 +0200

    MMT_2020_saukrs_laborinis_nr_2a.m: pradinis kodas

--- Semestras-1/Matematinio-modeliavimo-technologijos/laboratoriai-darbai/Saulius-Krasuckas/MMT_2020_saukrs_laborinis_nr_2a.m
+++ Semestras-1/Matematinio-modeliavimo-technologijos/laboratoriai-darbai/Saulius-Krasuckas/MMT_2020_saukrs_laborinis_nr_2a.m
@@ -1,3 +1,11 @@
-function MMT_2020_saukrs_laborinis_nr_2a.m
+function MMT_2020_saukrs_laborinis_nr_2a
     clc;
+
+    % skirtingi taškų rinkiniai Lagranžui:
+    x2 = linspace(1, 5, 2);
+    x3 = linspace(1, 5, 3);
+    x5 = linspace(1, 5, 5);
+    x5 = linspace(1, 5, 7);
+    x5 = linspace(1, 5, 9);
+
 end
