1e8f73b2cdc8d1b0730e3880cda354c633b99478
saulius-krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-01-26 11:15:07 +0200

    Update MMT_2020_saukrs_laborinis_nr_2b.m

--- Semestras-1/Matematinio-modeliavimo-technologijos/laboratoriai-darbai/Saulius-Krasuckas/MMT_2020_saukrs_laborinis_nr_2b.m
+++ Semestras-1/Matematinio-modeliavimo-technologijos/laboratoriai-darbai/Saulius-Krasuckas/MMT_2020_saukrs_laborinis_nr_2b.m
@@ -21,6 +21,8 @@ function MMT_2020_saukrs_laborinis_nr_2a
 
     % Iš failo nuskaitome vektorius x1 ir yd1:
     load('data1.mat');
+
+    % Tikrinu x1:
     fprintf('x1(1) = %f\n', x1(1));
     fprintf('x1(end) = %f\n', x1(end));
     fprintf('size(x1) = %s\n', mat2str(size(x1)));
@@ -28,10 +30,17 @@ function MMT_2020_saukrs_laborinis_nr_2a
     % x1(end) = 10.000000
     % size(x1) = [1 100]
 
+    % Fiksuoju x stulpelio pavidalu:
+    x = x1';
+
     % Lygčių sistemai paruošiu matricą A iš 
     % nuskaitytų x1 duomenų pirmame stulpelyje:
-    A(:, 1) = x1';
+    A(:, 1) = x;
     % ... ir vienetų antrame stulpelyje, formulė (3.8.2):
     A(:, 2) = ones(size(x1));
 
+    % Ištiesinu x į x':
+    x_ = 1 ./ x;
+
+    theta = A \ x_;
 end % of program.
