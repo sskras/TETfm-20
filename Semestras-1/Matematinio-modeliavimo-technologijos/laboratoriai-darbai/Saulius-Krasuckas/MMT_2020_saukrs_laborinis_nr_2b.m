445d71aaca7bcaf6d28022d0365f7f52afda0492
saulius-krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-01-26 10:45:17 +0200

    Update MMT_2020_saukrs_laborinis_nr_2b.m

--- Semestras-1/Matematinio-modeliavimo-technologijos/laboratoriai-darbai/Saulius-Krasuckas/MMT_2020_saukrs_laborinis_nr_2b.m
+++ Semestras-1/Matematinio-modeliavimo-technologijos/laboratoriai-darbai/Saulius-Krasuckas/MMT_2020_saukrs_laborinis_nr_2b.m
@@ -1,1 +1,24 @@
+function MMT_2020_saukrs_laborinis_nr_2a
 
+    % Prijungiu funkcijas iš dėstytojo modulių (iš .zip):
+    addpath('approx');
+
+    % Duotoji funkcija (93052026 mod 3) yra #0:
+    %
+    %     y = a/x + b;
+    %
+    % „Ištiesinta" versija:
+    %
+    %     y = ax' + b;
+    %
+    % Pakeitimas:
+    %
+    %     x' = 1/x;
+    %
+    % Duomenų failas:
+    %
+    %     "data1.mat"
+
+    
+
+end % of program.
