59460ce42d3d70dc445f1605f9c4e74ec0fbd3ae
saulius-krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-04-08 18:35:14 +0300

    Update zoom-paskaita-s2.3-4.cmd

--- Bendra/tools/zoom-paskaita-s2.3-4.cmd
+++ Bendra/tools/zoom-paskaita-s2.3-4.cmd
@@ -1,2 +1,3 @@
 @echo OFF
+: startuojame 3-iadienį ir 4-adienį pas Luką Pavilanską:
 %LOCALAPPDATA%\Programs\Zoom\bin\zoom.exe --url="https://us04web.zoom.us/j/2197442202?pwd=K0lNUURGY0FWNWVtbldzUHFYQUdKZz09" 
