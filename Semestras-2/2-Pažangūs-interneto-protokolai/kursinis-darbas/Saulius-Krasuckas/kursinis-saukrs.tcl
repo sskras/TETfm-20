5a483f6286f323a85cee0b87c96330a26033a61a
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-08 21:16:39 +0300

    kursinis-saukrs.tcl: Uždarymo procedūra

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs.tcl
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs.tcl
@@ -4,3 +4,16 @@ set ns [new Simulator]
 # Generuosime TR-formato treisus:
 set nf [open kursinis-saukrs.tcl w]
 $ns trace-all $nf
+
+# Prireiks uždarymo procedūros:
+proc finish {} {
+    # pasiimam pora globalių kintamųjų:
+    global ns nf
+    # išsaugom treiso likučius į failą:
+    $ns flush-trace
+    # uždarom treisą:
+    close $nf
+    # startuojam vizualizaciją
+    # TODO ateičiai
+    exit 0
+}
