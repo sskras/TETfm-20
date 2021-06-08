758c8ae958fd9e13a4891dd8552fbc4419ff01d3
saulius-krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-08 21:04:39 +0300

    Update simple.tcl

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/tools/NS-2/simple.tcl
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/tools/NS-2/simple.tcl
@@ -7,7 +7,6 @@ BEGIN {
 {
 	# Trace line format: normal
 	if ($2 != "-t") {
-	#	print "Br1: " $0
 		event = $1
 		time = $2
 		pkt_size = $6
@@ -16,22 +15,18 @@ BEGIN {
 	# Init prevTime to the first packet recv time
 	if(prevTime == 0)
 	{
-	#	print "Br2: " $0
 		prevTime = time
 	}
 
 	# # calculating throughput per time interval tic
 	if ( event == "r" ) {
 		
-	#	print "Br3: " $0
 		# adding received packet sizes
 		recv += pkt_size
 		currTime += (time - prevTime)
 		# if time value is bigger then tic calculate throughput ant print it
 		if (currTime >= tic) {
-	#		print "Br4: " $0
 			printf("%.2fs: %2.2f Mbps\n", time, (recv/currTime)*8/1000000)
-			recv = 0
 			currTime = 0
 		}
 		prevTime = time
