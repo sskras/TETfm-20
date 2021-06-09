834fafe60a19a1dd93680fa67446b2f220bcb451
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-09 12:39:36 +0300

    kursinis-saukrs.tcl: Išjungiu UDP siųstuvą antrame node

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs.tcl
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs.tcl
@@ -89,11 +89,9 @@ $cbr2 set rate_ 1mb
 $cbr2 set random_ false
 
 # Sudarome tinklo įvykių grafiką (vėlgi pagal pvz.):
-$ns at 0.1 "$cbr2 start"
 $ns at 0.0 "$ftp2 start"
 $ns at 1.0 "$ftp1 start"
 $ns at 4.0 "$ftp1 stop"
-$ns at 4.5 "$cbr2 stop"
 $ns at 4.9 "$ftp2 stop"
 
 # Įvykdome uždarymo procedūrą praėjus 5s simuliacijos laiko:
