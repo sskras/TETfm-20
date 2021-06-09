59cee2c4055f39bb5b06061d2f7a7879aa9f2f84
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-09 11:56:01 +0300

    kursinis-saukrs.tcl: Komentarų kėlimas į eilučių pabaigas

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs.tcl
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs.tcl
@@ -33,39 +33,37 @@ $ns queue-limit $node_parinktuvas $node_imtuvas 10
 # Sukuriame vieną TCP srauto šaltinį:
 set tcp_source_1 [new Agent/TCP]
 $tcp_source_1 set class_ 2
-$tcp_source_1 set fid_ 1            ; # priskiriam Flow-id
+$tcp_source_1 set fid_ 1                ; # priskiriam Flow-id
       # BUG: jei "fid_" priskiriame prieš "class_", Trace-faile Flow-id tampa = 2
-$node_siustuvas_1 attach $tcp_source_1  ; # Prijungiame srauto šaltinį prie siustuvo_1:
+$node_siustuvas_1 attach $tcp_source_1  ; # Prijungiame jį prie siustuvo_1
 
 # Sukuriame vieną TCP srauto imtuvą:
 set tcp_destination [new Agent/TCPSink]
-$node_imtuvas attach $tcp_destination
+$node_imtuvas attach $tcp_destination   ; # Prijungiame jį prie imtuvo nodo
 
 # Sujungiame TCP agentus tarp "siustuvas_1" ir "imtuvas":
 $ns connect $tcp_source_1 $tcp_destination
 
-# Prisegame FTP užpildą (Payload) prie pirmojo TCP srauto šaltinio:
+# Sukuriame FTP užpildą (Payload):
 set ftp1 [new Application/FTP]
-$ftp1 attach-agent $tcp_source_1
+$ftp1 attach-agent $tcp_source_1        ; # Prisegame jį prie TCP šaltinio
 $ftp1 set type_ FTP
 
-# Sukuriame vieną UDP srauto šaltinį:
+# Sukuriame UDP srauto šaltinį:
 set udp_source_2 [new Agent/UDP]
 $udp_source_2 set fid_ 2            ; # priskiriam Flow-id
-
-# Prijungiame šį srauto šaltinį prie siustuvo_2:
-$node_siustuvas_2 attach $udp_source_2
+$node_siustuvas_2 attach $udp_source_2  ; # Prijungiame jį prie siustuvo_2
 
 # Sukuriame UDP srauto imtuvą (per Null-agentą su turbūt begaliniu pralaidumu):
 set udp_destination [new Agent/Null]
-$node_imtuvas attach $udp_destination
+$node_imtuvas attach $udp_destination   ; # Prijungiame jį prie imtuvo nodo
 
 # Sujungiame UDP agentus tarp "siustuvas_2" ir "imtuvas":
 $ns connect $udp_source_2 $udp_destination
 
-# Prisegame CBR užpildą (Payload) prie UDP srauto šaltinio:
+# Sukuriame CBR užpildą (Payload):
 set cbr2 [new Application/Traffic/CBR]
-$cbr2 attach-agent $udp_source_2
+$cbr2 attach-agent $udp_source_2        ; # Prisegame jį prie UDP šaltinio
 $cbr2 set type_ CBR
 $cbr2 set packet_size_ 1000
 $cbr2 set rate_ 1mb
