f72227a52b7ecc00d1bc49bdc490ed34baa7c628
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-21 14:57:25 +0300

    kursinis-saukrs-run.sh: Įskaitomesnis prefiksas

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -4,4 +4,4 @@ echo "$0: Startuoju, cmd-line: "$*
 
 FNAME_PREFIX="saukrs-bandomasis"
 
-ns kursinis-saukrs.tcl -- 2ms 0.00 $FNAME_PREFIX"_2ms_0%-loss_HSTCP_+_BIC"
+ns kursinis-saukrs.tcl -- 2ms 0.00 $FNAME_PREFIX"_HSTCP_+_BIC,_2ms_0%-loss"
