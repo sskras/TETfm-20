3fcfd75ec16eeddc5163997846317dff6487f9e3
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-21 15:35:02 +0300

    kursinis-saukrs-run.sh: Keliu abu žingsnius į ciklą

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -8,11 +8,13 @@ FNAME_PREFIX="saukrs-bandomasis"
 
 DELAY="2"; LOSS="0.00"
 echo "$LOSS * 100" | bc -q | xargs printf "%d\n" | read LOSS_P
-ns kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} "${FNAME_PREFIX}_HSTCP_+_BIC,_${DELAY}ms_${LOSS_P}%-loss"
-echo
 
-DELAY="4"; LOSS="0.00"
-ns kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} "${FNAME_PREFIX}_HSTCP_+_BIC,_${DELAY}ms_${LOSS_P}%-loss"
+for DELAY in 2 4; do
+    echo "Skaičiuoju pagal \$DELAY=$DELAY"
+    ns kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} "${FNAME_PREFIX}_HSTCP_+_BIC,_${DELAY}ms_${LOSS_P}%-loss"
+    echo
+done
+
 echo
 
 ls -l *.{tr,nam}
