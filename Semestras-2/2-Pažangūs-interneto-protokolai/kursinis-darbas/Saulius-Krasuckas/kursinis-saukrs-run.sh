140808ec4f8b3c4edeeab87b6b31fa0ecfea07e0
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-21 15:29:28 +0300

    kursinis-saukrs-run.sh: Abiejuose paleidimuose naudoju ir , ir 0.01

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+shopt -s lastpipe
+
 echo "$0: Startuoju, cmd-line: "$*
 
 FNAME_PREFIX="saukrs-bandomasis"
 
+echo "$LOSS * 100" | bc -q | xargs printf "%d\n" | read LOSS_P
 DELAY="2"; LOSS="0.00"
-ns kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} "${FNAME_PREFIX}_HSTCP_+_BIC,_${DELAY}ms_0%-${LOSS}"
+ns kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} "${FNAME_PREFIX}_HSTCP_+_BIC,_${DELAY}ms_${LOSS_P}%-loss"
 echo
+
 DELAY="4"; LOSS="0.00"
-ns kursinis-saukrs.tcl -- 4ms 0.00 $FNAME_PREFIX"_HSTCP_+_BIC,_4ms_0%-loss"
+ns kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} "${FNAME_PREFIX}_HSTCP_+_BIC,_${DELAY}ms_${LOSS_P}%-loss"
 echo
 
 ls -l *.{tr,nam}
