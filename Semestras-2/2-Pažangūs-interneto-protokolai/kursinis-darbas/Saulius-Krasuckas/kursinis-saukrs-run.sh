06a5080a7c7dcad2ffafdc3b49684b9a87f9b55d
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-21 21:26:17 +0300

    kursinis-saukrs-run.sh: Grąžinu (atitaisau) modeliavimo trukmę pagal užduotį

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -11,13 +11,13 @@ echo "$LOSS * 100" | bc -q | xargs printf "%d\n" | read LOSS_P
 
 for DELAY in 0 2 4; do
     echo "Skaičiuoju pagal \$DELAY=$DELAY"
-#   ns $DIR/kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} "kursinis-saukrs" \
-    ns $DIR/kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} "${FNAME_PREFIX}_HSTCP_+_BIC,_${DELAY}ms_${LOSS_P}%-loss" \
+#   ns $DIR/kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} "${FNAME_PREFIX}_HSTCP_+_BIC,_${DELAY}ms_${LOSS_P}%-loss" \
+    ns $DIR/kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} "kursinis-saukrs" \
         0.1 '$ftp1 start' \
         0.2 '$ftp2 start' \
-        2.90 '$ftp1 stop'  \
-        2.90 '$ftp2 stop'  \
-        3.00 'finish'
+        290 '$ftp1 stop'  \
+        290 '$ftp2 stop'  \
+        300 'finish'
     echo
 done
 
