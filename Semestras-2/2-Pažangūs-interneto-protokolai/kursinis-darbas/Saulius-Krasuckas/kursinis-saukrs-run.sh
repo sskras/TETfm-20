1e05f74298b03f7d04cb9f2e35aa39ad3e88daf9
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-21 15:03:24 +0300

    kursinis-saukrs-run.sh: Antras treisas (padidintas vėlinimas)

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -6,6 +6,8 @@ FNAME_PREFIX="saukrs-bandomasis"
 
 ns kursinis-saukrs.tcl -- 2ms 0.00 $FNAME_PREFIX"_HSTCP_+_BIC,_2ms_0%-loss"
 echo
+ns kursinis-saukrs.tcl -- 4ms 0.00 $FNAME_PREFIX"_HSTCP_+_BIC,_4ms_0%-loss"
+echo
 
 ls -l *.{tr,nam}
 echo "Trinam?"; read
