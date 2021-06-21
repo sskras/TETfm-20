11b39fc43b7ccf514f101cfdf6402391596516b9
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-21 15:06:02 +0300

    kursinis-saukrs-run.sh: Pradedu vėlinimą dėti į kintamąjį

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -4,7 +4,8 @@ echo "$0: Startuoju, cmd-line: "$*
 
 FNAME_PREFIX="saukrs-bandomasis"
 
-ns kursinis-saukrs.tcl -- 2ms 0.00 $FNAME_PREFIX"_HSTCP_+_BIC,_2ms_0%-loss"
+DELAY="2"
+ns kursinis-saukrs.tcl -- $DELAY"ms" 0.00 "${FNAME_PREFIX}_HSTCP_+_BIC,_${DELAY}ms_0%-loss"
 echo
 ns kursinis-saukrs.tcl -- 4ms 0.00 $FNAME_PREFIX"_HSTCP_+_BIC,_4ms_0%-loss"
 echo
