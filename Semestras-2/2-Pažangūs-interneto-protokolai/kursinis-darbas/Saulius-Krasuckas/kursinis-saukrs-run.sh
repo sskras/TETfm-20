3cc42e14300028149e418a00acc8211e4543d1a9
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-21 15:44:29 +0300

    kursinis-saukrs-run.sh: Susimuliuokime tris vėlinimus

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -5,17 +5,14 @@ shopt -s lastpipe
 echo "$0: Startuoju, cmd-line: "$*
 
 FNAME_PREFIX="saukrs-bandomasis"
-
 LOSS="0.00"
 echo "$LOSS * 100" | bc -q | xargs printf "%d\n" | read LOSS_P
 
-for DELAY in 2 4; do
+for DELAY in 0 2 4; do
     echo "Skaičiuoju pagal \$DELAY=$DELAY"
     ns kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} "${FNAME_PREFIX}_HSTCP_+_BIC,_${DELAY}ms_${LOSS_P}%-loss"
     echo
 done
-
-echo
 
 ls -l *.{tr,nam}
 echo "Trinam?"; read
