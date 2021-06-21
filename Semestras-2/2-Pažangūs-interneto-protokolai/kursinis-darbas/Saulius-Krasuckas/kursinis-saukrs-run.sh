01282073b52b035d24f3a007a82cc9bc8729b52e
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-21 15:02:01 +0300

    kursinis-saukrs-run.sh: Trace-failų valymas

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -5,3 +5,8 @@ echo "$0: Startuoju, cmd-line: "$*
 FNAME_PREFIX="saukrs-bandomasis"
 
 ns kursinis-saukrs.tcl -- 2ms 0.00 $FNAME_PREFIX"_HSTCP_+_BIC,_2ms_0%-loss"
+echo
+
+ls -l *.{tr,nam}
+echo "Trinam?"; read
+rm -v *.{tr,nam}
