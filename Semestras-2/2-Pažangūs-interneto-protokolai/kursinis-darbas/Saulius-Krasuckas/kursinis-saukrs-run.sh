206572c25c15e19fa2e684c776c759f67182db4f
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-21 15:08:07 +0300

    kursinis-saukrs-run.sh: Nuostolius jau saugomi kintamajame

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -5,7 +5,7 @@ echo "$0: Startuoju, cmd-line: "$*
 FNAME_PREFIX="saukrs-bandomasis"
 
 DELAY="2"; LOSS="0.00"
-ns kursinis-saukrs.tcl -- $DELAY"ms" 0.00 "${FNAME_PREFIX}_HSTCP_+_BIC,_${DELAY}ms_0%-loss"
+ns kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} "${FNAME_PREFIX}_HSTCP_+_BIC,_${DELAY}ms_0%-${LOSS}"
 echo
 ns kursinis-saukrs.tcl -- 4ms 0.00 $FNAME_PREFIX"_HSTCP_+_BIC,_4ms_0%-loss"
 echo
