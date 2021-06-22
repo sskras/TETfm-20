0282c61558e629cd98fb93b337e3711e86b433b9
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-22 14:45:18 +0300

    kursinis-saukrs-run.sh: Pradedam adresuoti failus iš vienu lygiu aukštesnės direktorijos

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -2,7 +2,7 @@
 
 shopt -s lastpipe
 
-DIR=$(dirname $0)
+DIR=$(builtin cd $(dirname $0)"/.."; pwd)
 FILE_PREFIX="kursinis-saukrs"
 
 LOSS="0.00"
@@ -11,7 +11,7 @@ echo "$LOSS * 100" | bc -q | xargs printf "%d\n" | read LOSS_P
 # Vėlinimas pagal kursinio darbo užduotį, ms:
 for DELAY in 2 6 80; do
 #   ns $DIR/kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} "kursinis-saukrs_HSTCP_+_BIC,_${DELAY}ms_${LOSS_P}%-loss" \
-    ns $DIR/kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} ${FILE_PREFIX} \
+    ns $DIR/Saulius-Krasuckas/kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} ${FILE_PREFIX} \
         0.1 '$ftp1 start' \
         0.2 '$ftp2 start' \
         2.0 '$ftp1 stop'  \
@@ -20,7 +20,7 @@ for DELAY in 2 6 80; do
     echo
     echo Išsitraukiame tiriamosios linijos duomenis:
     ls -l kursinis-saukrs.tr
-    cat ${FILE_PREFIX}.tr | grep '^r .* 2 3' | awk -f $DIR/../tools/NS-2/Throughput.awk 2>&1 1>kursinis-saukrs.throughput.txt
+    cat ${FILE_PREFIX}.tr | grep '^r .* 2 3' | awk -f $DIR/tools/NS-2/Throughput.awk 2>&1 1>kursinis-saukrs.throughput.txt
     # TODO: atskirti reziume spausdinimą ir packet_size(t) ištraukimą į stderr + stdout
     echo
 done
