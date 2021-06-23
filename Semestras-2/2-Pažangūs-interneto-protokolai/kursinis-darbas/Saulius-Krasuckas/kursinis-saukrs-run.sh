1b988925170fbb6b91677c3dae875c5f4d4a52bd
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-23 22:55:25 +0300

    kursinis-saukrs-run.sh: Komentarų aptvarkymas

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -14,25 +14,24 @@ OUT_DIAGRAM_1="${FILE_PREFIX}-0%.throughput-by-time.png"
 # Išvalome logą:
 > ${OUT_SIMUL_LOG}
 
-# $ftp1 veikia steke su CC-algoritmu HSTCP
-# $ftp2 veikia steke su CC-algoritmu BIC
-
 xthr () # eXtract THRoughput: funkcija ištraukia pralaidumą tiriamojoje linijoje tarp Node 2 ir Node 3 (parinktuvų)
 {
     cat ${TMP_TRACEFILE} | grep '^r .* 2 3' | awk -f ${SCRIPT_AWK} 2>&1 1>${1}
 }
 
+# $ftp1 veikia steke su CC-algoritmu HSTCP
+# $ftp2 veikia steke su CC-algoritmu BIC
+
 read -r -d '' RUN_HSTCP_AND_BIC << \
--------------------------
+----------------------
     0.1 '\$ftp1 start'
     0.1 '\$ftp2 start'
     2.8 '\$ftp1 stop'
     2.8 '\$ftp2 stop'
     3.0 'finish'
--------------------------
-echo $CMD2
+----------------------
 
-# Vėlinimas pagal kursinio darbo užduotį, ms:
+# Keičiu vėlinimą pagal kursinio darbo užduotį:
 (eval ns ${SCRIPT_TCL} --  "2ms" 0.00 ${TMP_TRACEFILE} ${RUN_HSTCP_AND_BIC}; xthr ${FILE_PREFIX}--2ms-0%.throughput) | tee -a ${OUT_SIMUL_LOG}
 (eval ns ${SCRIPT_TCL} --  "6ms" 0.00 ${TMP_TRACEFILE} ${RUN_HSTCP_AND_BIC}; xthr ${FILE_PREFIX}--6ms-0%.throughput) | tee -a ${OUT_SIMUL_LOG}
 (eval ns ${SCRIPT_TCL} -- "80ms" 0.00 ${TMP_TRACEFILE} ${RUN_HSTCP_AND_BIC}; xthr ${FILE_PREFIX}-80ms-0%.throughput) | tee -a ${OUT_SIMUL_LOG}
