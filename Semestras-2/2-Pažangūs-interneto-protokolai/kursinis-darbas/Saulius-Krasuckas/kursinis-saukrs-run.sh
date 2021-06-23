d78ff9ece580e88e7a65de053341e3eb6e56f873
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-23 17:48:14 +0300

    kursinis-saukrs-run.sh: Tęsiu kintamųjų konsolidavimą

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -4,6 +4,9 @@ shopt -s lastpipe
 
 DIR=$(builtin cd $(dirname $0)"/.."; pwd)
 FILE_PREFIX="kursinis-saukrs"
+SCRIPT_TCL="$DIR/Saulius-Krasuckas/kursinis-saukrs.tcl"
+SCRIPT_GPL="$DIR/Saulius-Krasuckas/kursinis-saukrs-throughput-by-delay.p"
+SCRIPT_AWK="$DIR/tools/NS-2/Throughput.awk"
 DIAGRAM1="kursinis-saukrs-0%.throughput-by-time.png"
 
 LOSS_P="0" #%
@@ -14,18 +17,18 @@ echo "$LOSS_P / 100" | bc -l | xargs printf "%.2f" | read LOSS
 
 # Vėlinimas pagal kursinio darbo užduotį, ms:
 for DELAY in 2 6 80; do
-    ns $DIR/Saulius-Krasuckas/kursinis-saukrs.tcl -- "${DELAY}ms" ${LOSS} ${FILE_PREFIX} \
+    ns ${SCRIPT_TCL} -- "${DELAY}ms" ${LOSS} ${FILE_PREFIX} \
         0.1 '$ftp1 start' \
         0.1 '$ftp2 start' \
         2.8 '$ftp1 stop'  \
         2.8 '$ftp2 stop'  \
         3.0 'finish'
-    cat ${FILE_PREFIX}.tr | grep '^r .* 2 3' | awk -f $DIR/tools/NS-2/Throughput.awk 2>&1 \
+    cat ${FILE_PREFIX}.tr | grep '^r .* 2 3' | awk -f ${SCRIPT_AWK} 2>&1 \
       1>${FILE_PREFIX}-${DELAY}ms-${LOSS_P}%.throughput
 done
 
 ls -l ${FILE_PREFIX}*.{tr,nam,throughput}
 rm -v ${FILE_PREFIX}*.{tr,nam}
 
-gnuplot -e 'file_out="'${DIAGRAM1}'"' $DIR/Saulius-Krasuckas/kursinis-saukrs-throughput-by-delay.p
+gnuplot -e 'file_out="'${DIAGRAM1}'"' ${SCRIPT_GPL}
 gio open ${DIAGRAM1}
