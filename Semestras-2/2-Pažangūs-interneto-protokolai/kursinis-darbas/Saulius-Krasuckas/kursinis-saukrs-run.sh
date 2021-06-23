2249878ee3bb8e8e265bff75e340217a725a8091
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-23 23:51:25 +0300

    kursinis-saukrs-run.sh: Dar vienas paprastesnis kintamojo pavadinimas

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -7,16 +7,16 @@ FILE_PREFIX="kursinis-saukrs"
 SCRIPT_NS2="$DIR/Saulius-Krasuckas/kursinis-saukrs.tcl"
 SCRIPT_PLT="$DIR/Saulius-Krasuckas/kursinis-saukrs-throughput-by-delay.p"
 SCRIPT_THR="$DIR/tools/NS-3/Throughput.awk"
-TMP_TRACEFILE="${FILE_PREFIX}.tr"
+TRACE="${FILE_PREFIX}.tr"
 LOG_S="${FILE_PREFIX}.log"
-OUT1="${FILE_PREFIX}-0%.throughput-by-time.png"
+OUT1= "${FILE_PREFIX}-0%.throughput-by-time.png"
 
 # Išvalome logą:
 > ${LOG_S}
 
 xthr () # eXtract THRoughput: funkcija ištraukia pralaidumą tiriamojoje linijoje tarp Node 2 ir Node 3 (parinktuvų)
 {
-    cat ${TMP_TRACEFILE} | grep '^r .* 2 3' | awk -f ${SCRIPT_THR} 2>&1 1>${1}
+    cat ${1} | grep '^r .* 2 3' | awk -f ${SCRIPT_THR} 2>&1 1>${2}
 }
 
 # $ftp1 veikia steke su CC-algoritmu HSTCP
@@ -35,14 +35,14 @@ read -r -d '' RUN_HSTCP_AND_BIC << \
 exec > >(tee -i ${LOG_S}) 2>&1
 
 # Keičiu vėlinimą pagal kursinio darbo užduotį:
-TH1a=${FILE_PREFIX}--2ms-0%.thr; eval ns ${SCRIPT_NS2} --  "2ms" 0.00 ${TMP_TRACEFILE} ${RUN_HSTCP_AND_BIC}; xthr ${TH1a}
-TH1b=${FILE_PREFIX}--6ms-0%.thr; eval ns ${SCRIPT_NS2} --  "6ms" 0.00 ${TMP_TRACEFILE} ${RUN_HSTCP_AND_BIC}; xthr ${TH1b}
-TH1c=${FILE_PREFIX}-80ms-0%.thr; eval ns ${SCRIPT_NS2} -- "80ms" 0.00 ${TMP_TRACEFILE} ${RUN_HSTCP_AND_BIC}; xthr ${TH1c}
+TH1a=${FILE_PREFIX}--2ms-0%.thr; eval ns ${SCRIPT_NS2} --  "2ms" 0.00 ${TRACE} ${RUN_HSTCP_AND_BIC}; xthr ${TRACE} ${TH1a}
+TH1b=${FILE_PREFIX}--6ms-0%.thr; eval ns ${SCRIPT_NS2} --  "6ms" 0.00 ${TRACE} ${RUN_HSTCP_AND_BIC}; xthr ${TRACE} ${TH1b}
+TH1c=${FILE_PREFIX}-80ms-0%.thr; eval ns ${SCRIPT_NS2} -- "80ms" 0.00 ${TRACE} ${RUN_HSTCP_AND_BIC}; xthr ${TRACE} ${TH1c}
 
 # Stabdau išvesties dubliavimą į logą:
 exec > /dev/tty 2>&1
 
 gnuplot -e 'out1="'${OUT1}'"' ${SCRIPT_PLT}                     # Braižome pirmą diagramą
 gio open ${OUT1}                                                # Atidarome pirmą diagramą
-rm -v ${TMP_TRACEFILE}*                                         # Ištriname tarpinius (Trace-) failus:
+rm -v ${TRACE}*                                                 # Ištriname tarpinius Trace-failus
 ls -l ${FILE_PREFIX}*                                           # Parodome sukurtus failus:
