c425e5cf6190237b80285bfdbbb0f21bee4b7ca3
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-24 12:28:54 +0300

    kursinis-saukrs-run.sh: Pervadinu trečiajai diagramai ir inputus

--- Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
+++ Semestras-2/2-Pažangūs-interneto-protokolai/kursinis-darbas/Saulius-Krasuckas/kursinis-saukrs-run.sh
@@ -40,9 +40,9 @@ TH1b=${FILE_PREFIX}--6ms-0%.thr; eval ns ${SCRIPT_NS2} --  "6ms" 0.00 ${TRACE} $
 TH1c=${FILE_PREFIX}-80ms-0%.thr; eval ns ${SCRIPT_NS2} -- "80ms" 0.00 ${TRACE} ${RUN_HSTCP_AND_BIC}; xthr ${TRACE} ${TH1c}
 
 # Keičiu paketų praradimą pagal kursinio darbo užduotį:
-TH2a=${FILE_PREFIX}--2ms-1%.thr; eval ns ${SCRIPT_NS2} --  "2ms" 0.01 ${TRACE} ${RUN_HSTCP_AND_BIC}; xthr ${TRACE} ${TH2a}
-TH2b=${FILE_PREFIX}--2ms-4%.thr; eval ns ${SCRIPT_NS2} --  "2ms" 0.04 ${TRACE} ${RUN_HSTCP_AND_BIC}; xthr ${TRACE} ${TH2b}
-TH2c=${FILE_PREFIX}--2ms-6%.thr; eval ns ${SCRIPT_NS2} --  "2ms" 0.06 ${TRACE} ${RUN_HSTCP_AND_BIC}; xthr ${TRACE} ${TH2c}
+TH3a=${FILE_PREFIX}--2ms-1%.thr; eval ns ${SCRIPT_NS2} --  "2ms" 0.01 ${TRACE} ${RUN_HSTCP_AND_BIC}; xthr ${TRACE} ${TH3a}
+TH3b=${FILE_PREFIX}--2ms-4%.thr; eval ns ${SCRIPT_NS2} --  "2ms" 0.04 ${TRACE} ${RUN_HSTCP_AND_BIC}; xthr ${TRACE} ${TH3b}
+TH3c=${FILE_PREFIX}--2ms-6%.thr; eval ns ${SCRIPT_NS2} --  "2ms" 0.06 ${TRACE} ${RUN_HSTCP_AND_BIC}; xthr ${TRACE} ${TH3c}
 
 exec > /dev/tty 2>&1                                            # Stabdau išvesties dubliavimą į logą
 
@@ -54,9 +54,9 @@ gnuplot -e                                 \
        ${SCRIPT_PLT}                                            # Braižome pirmą diagramą
 
 gnuplot -e                                       \
-'in1="'${TH2a}'"; tt1="Paketų praradimas:  1%"; '\
-'in2="'${TH2b}'"; tt2="Paketų praradimas:  4%"; '\
-'in3="'${TH2c}'"; tt3="Paketų praradimas:  6%"; '\
+'in1="'${TH3a}'"; tt1="Paketų praradimas:  1%"; '\
+'in2="'${TH3b}'"; tt2="Paketų praradimas:  4%"; '\
+'in3="'${TH3c}'"; tt3="Paketų praradimas:  6%"; '\
 'out="'${OUT3}'"; pav="Pralaidumas, sukuriamas panaudojus HSTCP+BIC, kai paketų vėlinimas = 2 ms"' \
        ${SCRIPT_PLT}                                            # Braižome antrą diagramą
 
