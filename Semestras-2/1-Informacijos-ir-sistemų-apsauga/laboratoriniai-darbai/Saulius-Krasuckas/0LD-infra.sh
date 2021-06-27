9820a272923cea14a594fade605c68a1ffb84af6
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-28 01:33:35 +0300

    0LD-infra.{sh,log}: OK, diskas su tokiu GUIDu jau panaudotas

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -9,12 +9,15 @@ vm_list () {
 exec > >(tee -i "${LOG_FILE}") 2>&1                         # Dubliuoju išvestį į logą
 
 # Ubuntu 21.04 Linux OS .vdi atvaizdas:
-cd $BASE_DIR/VMs
+# cd $BASE_DIR/VMs
 # curl -OLv https://sourceforge.net/projects/osboxes/files/v/vb/55-U-u/21.04/64bit.7z/download
-time 7za x 64bit.7z
-cd -
+# time 7za x 64bit.7z
+# cd -
 
 ls -Al $BASE_DIR/VMs/64bit
+
+# Patikrinkim disko informaciją:
+VBoxManage showmediuminfo disk VMs/2021-VGTU-IiSA-LDVM1/2021-VGTU-IiSA-LDVM1.vdi
 
 # VM sąrašas:
 vm_list
@@ -22,6 +25,7 @@ vm_list
 # Kuriu 1LD mašiną:
 VBoxManage createvm --name VGTU-2021-IiSA-saukrs-LDVM1 --ostype Ubuntu_64 --basefolder $BASE_DIR/VMs/ --register
 vm_list
+# VM konfigūracija:
 VBoxManage showvminfo VGTU-2021-IiSA-saukrs-LDVM1
 vm_list
 
