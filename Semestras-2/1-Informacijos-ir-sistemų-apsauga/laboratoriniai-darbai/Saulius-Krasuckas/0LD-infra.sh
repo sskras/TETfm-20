cc475b27d1880de61b7f8624b90f8d33d3e29901
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-27 23:43:08 +0300

    0LD-infra.{sh,log}: Pradedu kelti darbinę direktoriją į atskirą kintamąjį

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-LOG_FILE=${0%.sh}.log                                       # Log failo vardas pagal skripto vardą (tik pakeičiu plėtinį)
+BASE_DIR=${0%.sh}                                           # Darbinė direktorija ten, kur skriptas
+LOG_FILE=${BASE_DIR}.log                                    # Log failo vardas pagal skripto vardą (tik pakeičiu plėtinį)
 
 exec > >(tee -i "${LOG_FILE}") 2>&1                         # Dubliuoju išvestį į logą
 
