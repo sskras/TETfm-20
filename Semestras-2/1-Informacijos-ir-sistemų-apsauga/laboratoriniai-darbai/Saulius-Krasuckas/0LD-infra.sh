13c945565200dee701022fbef50ba0dc6f10d84d
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-27 22:40:59 +0300

    0LD-infra.{sh,log}: Įjungiu infros paruošimo skripto sloginimą

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+exec > >(tee -i 0LD-infra.log) 2>&1                         # Dubliuoju išvestį į logą
+exec > /dev/tty 2>&1                                        # Stabdau išvesties dubliavimą
