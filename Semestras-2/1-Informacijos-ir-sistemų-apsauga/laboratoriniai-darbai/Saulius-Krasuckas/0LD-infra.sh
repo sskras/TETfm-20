8bd425b2e762334781ae3d7cbb524e1df41118e2
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-27 23:39:48 +0300

    0LD-infra.{sh,log}: Vis tiek ne CWD sukūrė, o namų direktorijoje

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -8,6 +8,7 @@ VBoxManage list vms | awk '{GUID=$NF; $NF=""; sub(/ $/, ""); print GUID" "$0}'
 
 # Kuriu 1LD mašiną:
 VBoxManage createvm --name VGTU-2021-IiSA-saukrs-LDVM1 --ostype Ubuntu_64 --basefolder ./VMs/ --register
+VBoxManage showvminfo VGTU-2021-IiSA-saukrs-LDVM1 | grep "Config file"
 
 # direktorija VM atvaizdams saugoti:
 pwd
