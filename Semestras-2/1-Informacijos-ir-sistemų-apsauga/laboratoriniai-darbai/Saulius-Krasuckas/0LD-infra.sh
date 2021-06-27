8670a5eb5e316d2e6963e443822ba36c5cb41dcd
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-27 22:53:56 +0300

    0LD-infra.{sh,log}: Panaudoju automatinį log failo vardą

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-LOG_FILE=0LD-infra.log
+LOG_FILE=${0%.sh}.log                                       # Log failo vardas pagal skripto vardą (tik pakeičiu plėtinį)
+
 exec > >(tee -i "${LOG_FILE}") 2>&1                         # Dubliuoju išvestį į logą
-echo ${0%.sh}.log
 VBoxManage list vms
 exec > /dev/tty 2>&1                                        # Stabdau išvesties dubliavimą
