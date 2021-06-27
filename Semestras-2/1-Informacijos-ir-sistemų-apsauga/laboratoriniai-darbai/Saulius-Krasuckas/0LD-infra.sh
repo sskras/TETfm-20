61f3d6df49646d48b8c851cb8822a3c043740c00
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-27 22:54:34 +0300

    0LD-infra.sh: Mašinų sąrašą prieš konstruojant naujas

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -2,5 +2,8 @@
 LOG_FILE=${0%.sh}.log                                       # Log failo vardas pagal skripto vardą (tik pakeičiu plėtinį)
 
 exec > >(tee -i "${LOG_FILE}") 2>&1                         # Dubliuoju išvestį į logą
+
+# VM sąrašas:
 VBoxManage list vms
+
 exec > /dev/tty 2>&1                                        # Stabdau išvesties dubliavimą
