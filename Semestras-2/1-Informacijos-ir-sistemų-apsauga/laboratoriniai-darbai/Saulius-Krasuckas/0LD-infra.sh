4c0d5d4cb134ae55547e3feddeea8f79a730cdf2
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-27 22:50:20 +0300

    0LD-infra.{sh,log}: Sukonstravau log failo vardą pagal skripto vardą

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LOG_FILE=0LD-infra.log
 exec > >(tee -i "${LOG_FILE}") 2>&1                         # Dubliuoju išvestį į logą
-echo ${0%.sh}
+echo ${0%.sh}.log
 VBoxManage list vms
 exec > /dev/tty 2>&1                                        # Stabdau išvesties dubliavimą
