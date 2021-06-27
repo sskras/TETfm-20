1bbc68dbd4e248590a26bf4528ba3b463c5427ea
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-28 01:01:07 +0300

    0LD-infra.{sh,log}: Nėra Ubuntu disko, bandau išsiarchyvuot

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -9,11 +9,11 @@ vm_list () {
 exec > >(tee -i "${LOG_FILE}") 2>&1                         # Dubliuoju išvestį į logą
 
 # Ubuntu 21.04 Linux OS .vdi atvaizdas:
-# cd $BASE_DIR
+cd $BASE_DIR
 # curl -OLv https://sourceforge.net/projects/osboxes/files/v/vb/55-U-u/21.04/64bit.7z/download
-# 7za x 64bit.6z
-# cd -
-ls -Al $BASE_DIR/64bit
+7za x 64bit.7z
+cd -
+ls -Al $BASE_DIR/VMs/64bit
 
 # VM sąrašas:
 vm_list
