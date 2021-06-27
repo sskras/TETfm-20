ae70577161e1a353b359dbc1d9d24235e4ec0f38
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-27 22:44:41 +0300

    0LD-infra.{sh,log}: Keliu logo vardą į kintamąjį

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+LOG_FILE=0LD-infra.log
 exec > >(tee -i 0LD-infra.log) 2>&1                         # Dubliuoju išvestį į logą
 VBoxManage list vms
 exec > /dev/tty 2>&1                                        # Stabdau išvesties dubliavimą
