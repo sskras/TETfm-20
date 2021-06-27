8920aa0a50a9b0f03b96ce09ae63faae1e81461d
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-27 22:49:15 +0300

    0LD-infra.{sh,log}: Bandau nutrint skripto vardo plėtinį

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LOG_FILE=0LD-infra.log
 exec > >(tee -i "${LOG_FILE}") 2>&1                         # Dubliuoju išvestį į logą
-echo $0
+echo ${0%.sh}
 VBoxManage list vms
 exec > /dev/tty 2>&1                                        # Stabdau išvesties dubliavimą
