8e3c74a63472b9c05b6bfdb316bc53687b505b37
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-28 09:49:16 +0300

    0LD-infra.{sh,log}: Tikrinu naujai išspaustą .vdi failą, neprieinu dėl senojo

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -18,7 +18,7 @@ ls -Al $BASE_DIR/VMs/64bit
 
 # Patikrinkim disko informaciją:
 VBoxManage showmediuminfo disk VMs/2021-VGTU-IiSA-LDVM1/2021-VGTU-IiSA-LDVM1.vdi
-VBoxManage showmediuminfo disk "VMs/Ubuntu 21.04 (64bit).vdi"
+VBoxManage showmediuminfo disk "VMs/64bit/Ubuntu 21.04 (64bit).vdi"
 
 # VM sąrašas:
 vm_list
