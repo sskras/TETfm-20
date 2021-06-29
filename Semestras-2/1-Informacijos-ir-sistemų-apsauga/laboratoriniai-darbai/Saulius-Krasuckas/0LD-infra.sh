39f0f779ea32d4a37c1f3da998eee8f125c39bdd
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-29 13:48:11 +0300

    0LD-infra.sh: Naujas vardas bendrai VM; indentuoju f-jų kodą

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -3,6 +3,7 @@
 BASE_DIR=$(builtin cd $(dirname $0); pwd)                   # Darbinė direktorija ten, kur skriptas
 LOG_FILE=${BASE_DIR}/$(basename ${0%.sh}).log               # Log failo vardas pagal skripto vardą (tik pakeičiu plėtinį)
 LOG_UART=${LOG_FILE%.log}-serial.log                        # Log failas VMų Serial/UART konsolei
+VM0="VGTU-2021-IiSA-saukrs-LDVM0"                           # Bendros VM vardas
 VM1="VGTU-2021-IiSA-saukrs-LDVM1"                           # Pirmos VM vardas
 
 shopt -s lastpipe
@@ -14,88 +15,88 @@ VBoxManage_vm_list () {
 }
 
 VBoxManage_createvm () {                                    # Kuriu VM atskiroje funkcijoje
-cd $BASE_DIR/VMs
+    cd $BASE_DIR/VMs
 
-# Šį failą reikėtų automatiškai pervadinti, kad netrukdytų curl raktui -J:
-# mv -bv 64bit.7z <TODO>
+    # Šį failą reikėtų automatiškai pervadinti, kad netrukdytų curl raktui -J:
+    # mv -bv 64bit.7z <TODO>
 
-# Ubuntu Desktop 21.04 Linux OS .vdi atvaizdas:
-# curl -OLv https://sourceforge.net/projects/osboxes/files/v/vb/55-U-u/21.04/64bit.7z/download
-#
-# Ubuntu Server 20.04.2 LTS .vdi atvaizdas:
-# curl -OLv https://sourceforge.net/projects/osboxes/files/v/vb/59-U-u-svr/20.04/20.04.2/64bit.7z/download
-#
-# Ubuntu Desktop 20.04.2 Focal Fossa (LTS) .vdi atvaizdas:
-# curl -OLJv https://sourceforge.net/projects/osboxes/files/v/vb/55-U-u/20.04/20.04.2/64bit.7z/download
+    # Ubuntu Desktop 21.04 Linux OS .vdi atvaizdas:
+    # curl -OLv https://sourceforge.net/projects/osboxes/files/v/vb/55-U-u/21.04/64bit.7z/download
+    #
+    # Ubuntu Server 20.04.2 LTS .vdi atvaizdas:
+    # curl -OLv https://sourceforge.net/projects/osboxes/files/v/vb/59-U-u-svr/20.04/20.04.2/64bit.7z/download
+    #
+    # Ubuntu Desktop 20.04.2 Focal Fossa (LTS) .vdi atvaizdas:
+    # curl -OLJv https://sourceforge.net/projects/osboxes/files/v/vb/55-U-u/20.04/20.04.2/64bit.7z/download
 
-# TODO rasti būdą kaip išsirinkti, kurį Image naudosime:
-# TODO (kol kas atstatau 64bit.7z kaip Symbolic Link)
-7za l 64bit.7z | awk '/vdi$/ {$1=$2=$3=$4=$5=""; print}' | read VDI_FILE
-echo "Ištrauktojo VDI disko pavadinimas: ${VDI_FILE}"
-# time 7za x 64bit.7z
-cd -
+    # TODO rasti būdą kaip išsirinkti, kurį Image naudosime:
+    # TODO (kol kas atstatau 64bit.7z kaip Symbolic Link)
+    7za l 64bit.7z | awk '/vdi$/ {$1=$2=$3=$4=$5=""; print}' | read VDI_FILE
+    echo "Ištrauktojo VDI disko pavadinimas: ${VDI_FILE}"
+    # time 7za x 64bit.7z
+    cd -
 
-ls -Al $BASE_DIR/VMs/64bit
+    ls -Al $BASE_DIR/VMs/64bit
 
-# Patikrinkim disko informaciją:
-# (ši užklausa automatiškai užregistruoja .vdi failą VBox registre, jei jo ten dar nebuvo:
-VBoxManage showmediuminfo disk "VMs/${VDI_FILE}"
-VBoxManage showmediuminfo disk "VMs/${VDI_FILE}" | awk '/^UUID/ {print $2}' | read VDI_UUID
-echo
+    # Patikrinkim disko informaciją:
+    # (ši užklausa automatiškai užregistruoja .vdi failą VBox registre, jei jo ten dar nebuvo:
+    VBoxManage showmediuminfo disk "VMs/${VDI_FILE}"
+    VBoxManage showmediuminfo disk "VMs/${VDI_FILE}" | awk '/^UUID/ {print $2}' | read VDI_UUID
+    echo
 
-# VM sąrašas:
-VBoxManage_vm_list
-echo
+    # VM sąrašas:
+    VBoxManage_vm_list
+    echo
 
-# Kuriu 1LD mašiną:
-VBoxManage createvm --name ${VM1} --ostype Ubuntu_64 --basefolder $BASE_DIR/VMs/ --register
-VBoxManage_vm_list
-echo
+    # Kuriu 1LD mašiną:
+    VBoxManage createvm --name ${VM1} --ostype Ubuntu_64 --basefolder $BASE_DIR/VMs/ --register
+    VBoxManage_vm_list
+    echo
 
-# VM konfigūracija:
-VBoxManage showvminfo ${VM1}
-echo "Sena procesoriaus ir RAM konfigūracija:"
-VBoxManage showvminfo ${VM1} | grep -e CPUs -e Memory
-echo
+    # VM konfigūracija:
+    VBoxManage showvminfo ${VM1}
+    echo "Sena procesoriaus ir RAM konfigūracija:"
+    VBoxManage showvminfo ${VM1} | grep -e CPUs -e Memory
+    echo
 
-# Padidinu CPU skaičių ir RAM apimtį, tik neturiu 4ių GiB šioje fizinėje mašinoje:
-# https://help.ubuntu.com/community/Installation/SystemRequirements
-VBoxManage modifyvm ${VM1} --cpus 2 --memory 2048
-echo "Nauja procesoriaus ir RAM konfigūracija:"
-VBoxManage showvminfo ${VM1} | grep -e CPUs -e Memory
-echo
+    # Padidinu CPU skaičių ir RAM apimtį, tik neturiu 4ių GiB šioje fizinėje mašinoje:
+    # https://help.ubuntu.com/community/Installation/SystemRequirements
+    VBoxManage modifyvm ${VM1} --cpus 2 --memory 2048
+    echo "Nauja procesoriaus ir RAM konfigūracija:"
+    VBoxManage showvminfo ${VM1} | grep -e CPUs -e Memory
+    echo
 
-# Prijungiu disko valdiklį:
-VBoxManage storagectl ${VM1} --name "SATA valdiklis" --add sata --bootable on
+    # Prijungiu disko valdiklį:
+    VBoxManage storagectl ${VM1} --name "SATA valdiklis" --add sata --bootable on
 
-# Prie valdiklio prijungiu išspaustą .VDI kaip naują diską:
-# TODO išsitraukti UUID iš naujai išspausto .VDI ir perduoti per CLI:
-echo "Nauja SATA konfigūracija:"
-VBoxManage storageattach ${VM1} --storagectl "SATA valdiklis" --port 0 --device 0 --type hdd --medium ${VDI_UUID}
-echo "Nauja diskų valdiklio konfigūracija:"
-VBoxManage showvminfo ${VM1} | grep -i storage
-echo
+    # Prie valdiklio prijungiu išspaustą .VDI kaip naują diską:
+    # TODO išsitraukti UUID iš naujai išspausto .VDI ir perduoti per CLI:
+    echo "Nauja SATA konfigūracija:"
+    VBoxManage storageattach ${VM1} --storagectl "SATA valdiklis" --port 0 --device 0 --type hdd --medium ${VDI_UUID}
+    echo "Nauja diskų valdiklio konfigūracija:"
+    VBoxManage showvminfo ${VM1} | grep -i storage
+    echo
 
-# Sukuriu Host-only interfeisą Host pusėje:
-VBoxManage hostonlyif create
-VBoxManage list hostonlyifs | awk '/^Name/ {NEWEST_NIC=$2} END {print NEWEST_NIC}' | read HOSTONLY_IF
-# Pasirenku bet kurį iš LD2 nurodytų IP adresų: 192.168.10.x:
-VBoxManage hostonlyif ipconfig ${HOSTONLY_IF} --ip 192.168.10.8
-echo "Naujas Host-only NIC:"
-VBoxManage list hostonlyifs | awk '/'${HOSTONLY_IF}'/ {START=1} START && $0=="" {START=0} START {print}'
-echo
+    # Sukuriu Host-only interfeisą Host pusėje:
+    VBoxManage hostonlyif create
+    VBoxManage list hostonlyifs | awk '/^Name/ {NEWEST_NIC=$2} END {print NEWEST_NIC}' | read HOSTONLY_IF
+    # Pasirenku bet kurį iš LD2 nurodytų IP adresų: 192.168.10.x:
+    VBoxManage hostonlyif ipconfig ${HOSTONLY_IF} --ip 192.168.10.8
+    echo "Naujas Host-only NIC:"
+    VBoxManage list hostonlyifs | awk '/'${HOSTONLY_IF}'/ {START=1} START && $0=="" {START=0} START {print}'
+    echo
 
-# Prijungiu jį prie NIC 2: (OAM valdymui per tinklą)
-VBoxManage modifyvm ${VM1} --nic2 hostonly --hostonlyadapter2 ${HOSTONLY_IF}
-echo "Nauja NIC konfigūracija:"
-VBoxManage showvminfo ${VM1} | grep NIC
-echo
+    # Prijungiu jį prie NIC 2: (OAM valdymui per tinklą)
+    VBoxManage modifyvm ${VM1} --nic2 hostonly --hostonlyadapter2 ${HOSTONLY_IF}
+    echo "Nauja NIC konfigūracija:"
+    VBoxManage showvminfo ${VM1} | grep NIC
+    echo
 
-# Prijungiu Serial UART: (valdymui be tinklo)
-VBoxManage modifyvm ${VM1} --uart1 "0x3f8" 4 --uartmode1 tcpserver 23001
-echo "Nauja UART konfigūracija:"
-VBoxManage showvminfo ${VM1} | grep UART
-echo
+    # Prijungiu Serial UART: (valdymui be tinklo)
+    VBoxManage modifyvm ${VM1} --uart1 "0x3f8" 4 --uartmode1 tcpserver 23001
+    echo "Nauja UART konfigūracija:"
+    VBoxManage showvminfo ${VM1} | grep UART
+    echo
 }
 
 # Ir jos dabar nebekviečiu, nes VM jau puikiai sudėliota
@@ -105,36 +106,36 @@ echo "Įjungiu ${VM1}:"
 VBoxManage startvm ${VM1}
 
 VBoxManage_setup_serial_console () {
-# ... ir ją pristabdau Boot Loader nustatymams:
-VBoxManage controlvm ${VM1} pause
-echo
-echo "VM lange Spauskite kombinaciją <Host-P>, tuomet <Esc>"
-echo "Kartokite <Esc> paspaudimus be perstojo."
-echo "Kai pasirodys GRUB meniu, spauskite <e>"
-echo "Eikite žemyn iki eilutės:"
-echo "        linux        /boot/vmlinuz-..."
-echo
-echo "Spauskite <End> ir gale prirašykite:"
-echo
-echo "... console=ttyS0,115200n8"
-echo
-echo "VM paspauskite <Ctrl-X>"
-echo
-echo "Iškart persijunkite čia (atgal į CLI)"
-echo "... ir spauskite <Enter>"
-echo
-echo "Prisijungsite prie Serial konsolės:"
+    # ... ir ją pristabdau Boot Loader nustatymams:
+    VBoxManage controlvm ${VM1} pause
+    echo
+    echo "VM lange Spauskite kombinaciją <Host-P>, tuomet <Esc>"
+    echo "Kartokite <Esc> paspaudimus be perstojo."
+    echo "Kai pasirodys GRUB meniu, spauskite <e>"
+    echo "Eikite žemyn iki eilutės:"
+    echo "        linux        /boot/vmlinuz-..."
+    echo
+    echo "Spauskite <End> ir gale prirašykite:"
+    echo
+    echo "... console=ttyS0,115200n8"
+    echo
+    echo "VM paspauskite <Ctrl-X>"
+    echo
+    echo "Iškart persijunkite čia (atgal į CLI)"
+    echo "... ir spauskite <Enter>"
+    echo
+    echo "Prisijungsite prie Serial konsolės:"
 
-read
+    read
 
-# Išvalau būsimą Serial logą:
-> screenlog.0
+    # Išvalau būsimą Serial logą:
+    > screenlog.0
 
-# Jungiamės prie virtualios Serial konsolės per TCP:
-screen -L telnet 127.0.0.1 23001
+    # Jungiamės prie virtualios Serial konsolės per TCP:
+    screen -L telnet 127.0.0.1 23001
 
-# Išsaugome Serial konsolės logą Plain-text formatu:
-cat screenlog.0 | ansifilter > ${LOG_UART}
+    # Išsaugome Serial konsolės logą Plain-text formatu:
+    cat screenlog.0 | ansifilter > ${LOG_UART}
 }
 
 # direktorija VM atvaizdams saugoti:
@@ -145,14 +146,14 @@ ls -Al VMs/VGTU-2021-IiSA-saukrs-LDVM*
 VBoxManage showvminfo ${VM1}
 
 VBoxManage_deletevm () {                                    # Naikinu VM irgi atskiroje funkcijoje
-echo "Trinam ${VM1} ?"
-read
+    echo "Trinam ${VM1} ?"
+    read
 
-VBoxManage unregistervm ${VM1}
-VBoxManage hostonlyif remove ${HOSTONLY_IF}
-# Išvalau individualius VM likučius:
-rm -rv ${BASE_DIR}/VMs/${VM1}
-VBoxManage_vm_list
-ls -Al VMs/
+    VBoxManage unregistervm ${VM1}
+    VBoxManage hostonlyif remove ${HOSTONLY_IF}
+    # Išvalau individualius VM likučius:
+    rm -rv ${BASE_DIR}/VMs/${VM1}
+    VBoxManage_vm_list
+    ls -Al VMs/
 }
 exec > /dev/tty 2>&1                                        # Stabdau išvesties dubliavimą
