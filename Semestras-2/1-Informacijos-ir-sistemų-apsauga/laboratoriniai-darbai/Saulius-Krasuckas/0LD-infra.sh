a7aea9f06ff046e4cd6f897f703e720aab5abc06
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-29 15:49:22 +0300

    0LD-infra.sh: Pamėginkime patikrinti .vdi atvaizdą prieš atkabinimą

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -165,9 +165,11 @@ VBoxManage startvm ${VM1}
 VBoxManage_start_LDVM1
 VBoxManage_show_vm_details
 
+    VBoxManage showmediuminfo disk ${VDI_UUID} | grep "In use by VMs"
+
     # Nuo valdiklio atjungiu .VDI atvaizdą/diską:
     VBoxManage storageattach ${VM1} --storagectl "SATA valdiklis" --port 0 --device 0 --medium "none"
     echo "SATA konfigūracija:"
-    VBoxManage showvminfo ${VM1} | grep -i storage
+    VBoxManage showvminfo ${VM1} | grep -i -e Storage -e SATA
 
 exec > /dev/tty 2>&1                                        # Stabdau išvesties dubliavimą
