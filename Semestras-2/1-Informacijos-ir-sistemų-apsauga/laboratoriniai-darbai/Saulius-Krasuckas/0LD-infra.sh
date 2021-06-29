908af6bf8d5f9a58212e0ad84eef4ac5b8036e54
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-29 21:46:23 +0300

    0LD-infra*: Daugiau debugo, aiškinuosi keliagubus Host-only IF

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -73,23 +73,26 @@ VBoxManage_createvm () {                                    # Kuriu VM atskiroje
     VBoxManage storagectl ${1} --name "SATA valdiklis" --add sata --bootable on
 
     # Prie valdiklio prijungiu išspaustą .VDI kaip naują diską:
-    echo "Nauja SATA konfigūracija:"
-    VBoxManage storageattach ${1} --storagectl "SATA valdiklis" --port 0 --device 0 --type hdd --medium ${VDI_UUID}
+   #echo "Nauja SATA konfigūracija:"
+   #VBoxManage storageattach ${1} --storagectl "SATA valdiklis" --port 0 --device 0 --type hdd --medium ${VDI_UUID}
     echo "Nauja diskų valdiklio konfigūracija:"
     VBoxManage showvminfo ${1} | grep -i storage
     echo
 
+    set -x
     # Sukuriu Host-only interfeisą Host pusėje:
     VBoxManage list hostonlyifs | awk '/^Name/ {NEWEST_NIC=$2} END {print NEWEST_NIC}' | read HOSTONLY_IF
     if [ -z "$HOSTONLY_OF" ]; then
         VBoxManage hostonlyif create
         VBoxManage list hostonlyifs | awk '/^Name/ {NEWEST_NIC=$2} END {print NEWEST_NIC}' | read HOSTONLY_IF
+        set +x
     fi
     # Pasirenku bet kurį iš LD2 nurodytų IP adresų: 192.168.10.x:
     VBoxManage hostonlyif ipconfig ${HOSTONLY_IF} --ip 192.168.10.8
     echo "Naujas Host-only NIC:"
     VBoxManage list hostonlyifs | awk '/'${HOSTONLY_IF}'/ {START=1} START && $0=="" {START=0} START {print}'
     echo
+    set +x
 
     # Prijungiu jį prie NIC 2: (OAM valdymui per tinklą)
     VBoxManage modifyvm ${1} --nic2 hostonly --hostonlyadapter2 ${HOSTONLY_IF}
