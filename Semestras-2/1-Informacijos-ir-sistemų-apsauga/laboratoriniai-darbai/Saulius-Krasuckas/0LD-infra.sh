17320f24dc17e155b36e0b9ff49bd678d2c42795
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-29 19:40:47 +0300

    0LD-infra.sh: Pagaliau .vdi atjungimo f-ja universali

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -186,7 +186,7 @@ VBoxManage_attach_VDI_to_VM () {
 
 VBoxManage_get_VDI_image
 echo "Rastas .vdi failas: ${VDI_UUID}"
-VBoxManage_detach_VDI_from_VM ${VM} ${VDI_UUID}
+VBoxManage_detach_VDI_from_VM ${VM1} ${VDI_UUID}
 VBoxManage modifyhd ${VDI_UUID} --type multiattach
 VBoxManage_attach_VDI_to_VM ${VM1} ${VDI_UUID}
 VBoxManage_start ${VM1}
