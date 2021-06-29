d7dd0f1a163c54a132ca9b827b79cffa04c9c758
Saulius Krasuckas <saulius.krasuckas@stud.vgtu.lt>
2021-06-29 03:03:19 +0300

    0LD-infra.*,sceenlog.0: Priskyriau Host-only NIC 2 statinį potinklį, nurodytą LD2

--- Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
+++ Semestras-2/1-Informacijos-ir-sistemų-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
@@ -79,7 +79,7 @@ echo
 VBoxManage hostonlyif create
 VBoxManage list hostonlyifs | awk '/^Name/ {NEWEST_NIC=$2} END {print NEWEST_NIC}' | read HOSTONLY_IF
 VBoxManage dhcpserver modify --interface=${HOSTONLY_IF} --disable
-VBoxManage hostonlyif ${HOSTONLY_IF} --ip 192.168.10.8
+VBoxManage hostonlyif ipconfig ${HOSTONLY_IF} --ip 192.168.10.8
 echo "Naujas Host-only NIC:"
 VBoxManage list hostonlyifs | awk '/'${HOSTONLY_IF}'/ {START=1} START && $0=="" {START=0} START {print}'
 echo
