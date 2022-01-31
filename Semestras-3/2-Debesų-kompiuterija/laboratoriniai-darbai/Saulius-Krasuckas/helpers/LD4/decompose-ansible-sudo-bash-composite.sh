52848e19c439350cf2c8652b731e6e8e80985169
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-01-31 14:05:58 +0200

    Saulius-Krasuckas/helpers: Konsoliduoju pagalbinius De-Ko LD4 failus (skriptus) į atskirą padirektorę

---
+++ Semestras-3/2-Debesų-kompiuterija/laboratoriniai-darbai/Saulius-Krasuckas/helpers/LD4/decompose-ansible-sudo-bash-composite.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sh -c '/bin/sh -c '"'"'sudo -H -S -n  -u root /bin/sh -c '"'"'"'"'"'"'"'"'echo BECOME-SUCCESS-xucyadvmmhrwwkflwsxapwvnhklmvrvo ; /usr/bin/python3 /home/stud/.ansible/tmp/ansible-tmp-1638899019.7293622-253348797528778/AnsiballZ_setup.py'"'"'"'"'"'"'"'"' && sleep 0'"'"''
+
+# =>
+
+    /bin/sh \
+            -c \
+	    "'"sudo -H -S -n  -u root /bin/sh -c '"'"'"'"'"'"'"'"'echo BECOME-SUCCESS-xucyadvmmhrwwkflwsxapwvnhklmvrvo ; /usr/bin/python3 /home/stud/.ansible/tmp/ansible-tmp-1638899019.7293622-253348797528778/AnsiballZ_setup.py'"'"'"'"'"'"'"'"' && sleep 0"'"
+
+# =>
+
+sudo \
+    -H \
+    -S \
+    -n \
+    -u root \
+    /bin/sh -c '\''echo BECOME-SUCCESS-xucyadvmmhrwwkflwsxapwvnhklmvrvo ; /usr/bin/python3 /home/stud/.ansible/tmp/ansible-tmp-1638899019.7293622-253348797528778/AnsiballZ_setup.py'\''
+&& sleep 0
