ba507aaa44e6bac443c1a9f80dfecac2975bf5a2
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-01-31 14:13:49 +0200

    helpers/Mi-Fi-router-get-WLAN-hosts.sh: JSON užklausos curl komanda, sugeneruoat Chrome naršyklės inspektoriuje

---
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
@@ -0,0 +1,9 @@
+curl 'http://192.168.1.1/api/model.json?internalapi=1&x=81601' \
+  -H 'Connection: keep-alive' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36' \
+  -H 'Accept: */*' \
+  -H 'Referer: http://192.168.1.1/index.html' \
+  -H 'Accept-Language: en-US,en;q=0.9,lt;q=0.8,ru;q=0.7' \
+  -H 'Cookie: p=password; sessionId=00000000-dbdNKPzOVdALgI15zV4VFA46QXRwkxh' \
+  --compressed \
+  --insecure
