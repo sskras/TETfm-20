df98a442caa2f20b156d24b9a52b79ad20a68194
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-01-31 17:14:50 +0200

    build-host*.*: Gražesnė išvestis galutiniame terminalo emuliatoriuje.
    Anksčiau 'Trying' jame prasidėdavo ne nuo eilutės pradžios.

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/build-host.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/build-host.sh
@@ -41,7 +41,7 @@ VBox_setup_serial_console () {
     > ${UART_SCR}
 
     # Jungiamės prie virtualios Serial konsolės per TCP:
-    script -c "telnet 127.0.0.1 23001" ${UART_SCR}
+    script -c "echo; telnet 127.0.0.1 23001" ${UART_SCR}
 
     # Išsaugome Serial konsolės logą Plain-text formatu:
     cat ${UART_SCR} | ansifilter |  sed '1d;$d' > ${UART_LOG}
