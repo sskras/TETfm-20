0303276aa428e6a6aef8bb4386e4a9033e5ff0ff
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-01-31 14:23:20 +0200

    helpers/Mi-Fi-router-get-WLAN-hosts.*: Iškerpu \r simbolius (Windows CR), trukdo

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
@@ -6,4 +6,5 @@ curl 'http://192.168.1.1/api/model.json?internalapi=1&x=81601' \
   -H 'Accept-Language: en-US,en;q=0.9,lt;q=0.8,ru;q=0.7' \
   -H 'Cookie: p=password; sessionId=00000000-dbdNKPzOVdALgI15zV4VFA46QXRwkxh' \
   --compressed \
-  --insecure
+  --insecure \
+  | sed 's/\r//'
