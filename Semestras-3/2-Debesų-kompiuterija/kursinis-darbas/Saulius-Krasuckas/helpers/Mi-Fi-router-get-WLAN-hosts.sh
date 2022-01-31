60ebcf2205f039a21d5dddb976ddde07eb80d681
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-01-31 14:34:06 +0200

    helpers/Mi-Fi-router-get-WLAN-hosts.sh: Paseno naršyklinė sesiją, sukūriau naują

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
@@ -1,5 +1,5 @@
 curl 'http://192.168.1.1/api/model.json?internalapi=1&x=81601' \
-  -H 'Cookie: p=password; sessionId=00000000-dbdNKPzOVdALgI15zV4VFA46QXRwkxh' \
+  -H 'Cookie: p=password; sessionId=00000001-yTg32I35Tsygf43pjCurMKR8O3P9bbf' \
   --compressed \
   --insecure \
   | sed 's/\r//'
