2942d5f03aac1b0d620e15310e9b50252493ffcd
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-01-31 16:53:05 +0200

    build-host.sh: Įkeliu praeito semestro UART loginimo funkciją

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/build-host.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/build-host.sh
@@ -9,7 +9,42 @@ BASE_DIR=$(builtin cd $(dirname $0); pwd)                   # Darbinė direktori
 LOG_FILE=${BASE_DIR}/$(basename ${0%.sh}).log               # Log failo vardas pagal skripto vardą (tik pakeičiu plėtinį)
 LOG_UART=${LOG_FILE%.log}-serial.log                        # Log failas VMų Serial/UART konsolei
 
+
 exec > >(tee -i "${LOG_FILE}") 2>&1                         # Dubliuoju išvestį į logą
+
+
+VBoxManage_setup_serial_console () {
+    # Pristabdau nurodytą VMą keisti jo Boot Loader nustatymams:
+    VBoxManage controlvm ${1} pause
+    echo
+    echo "VM lange Spauskite kombinaciją <Host-P>, tuomet <Esc>"
+    echo "Kartokite <Esc> paspaudimus be perstojo."
+    echo "Kai pasirodys GRUB meniu, spauskite <e>"
+    echo "Eikite žemyn iki eilutės:"
+    echo "        linux        /boot/vmlinuz-..."
+    echo
+    echo "Spauskite <End> ir gale prirašykite:"
+    echo
+    echo "... console=ttyS0,115200n8"
+    echo
+    echo "VM paspauskite <Ctrl-X>"
+    echo
+    echo "Iškart persijunkite čia (atgal į CLI)"
+    echo "... ir spauskite <Enter>"
+    echo
+    echo "Prisijungsite prie Serial konsolės:"
+
+    read
+
+    # Išvalau būsimą Serial logą:
+    > screenlog.0
+
+    # Jungiamės prie virtualios Serial konsolės per TCP:
+    screen -L telnet 127.0.0.1 23001
+
+    # Išsaugome Serial konsolės logą Plain-text formatu:
+    cat screenlog.0 | ansifilter > ${LOG_UART}
+}
 
 echo "$(basename $0): Startuojama infrastruktūra"
 
