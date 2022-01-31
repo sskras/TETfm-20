ff3dba0a38cc6416edbe7f01d45ed939cbc6b222
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-01-31 14:35:35 +0200

    helpers/Mi-Fi-router-get-WLAN-hosts.sh: Išsikerpu tik WLAN stotelių duomenis

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
@@ -2,4 +2,5 @@ curl 'http://192.168.1.1/api/model.json?internalapi=1&x=81601' \
   -H 'Cookie: p=password; sessionId=00000001-yTg32I35Tsygf43pjCurMKR8O3P9bbf' \
   --compressed \
   --insecure \
-  | sed 's/\r//'
+  | sed 's/\r//' \
+  | /mingw64/bin/jq '.router.clientList'
