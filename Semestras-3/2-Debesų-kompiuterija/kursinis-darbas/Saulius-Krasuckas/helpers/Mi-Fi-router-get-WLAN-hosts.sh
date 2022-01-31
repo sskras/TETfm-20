9ae391218b3d2ed33be9f0c17ec2f1cad7f61797
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-01-31 14:37:15 +0200

    helpers/Mi-Fi-router-get-WLAN-hosts.sh: WLAN stotelių duomenų pvz.

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
@@ -4,3 +4,23 @@ curl 'http://192.168.1.1/api/model.json?internalapi=1&x=81601' \
   --insecure \
   | sed 's/\r//' \
   | /mingw64/bin/jq '.router.clientList'
+
+# jq iškerpta tokį gabalėlį:
+#
+# [
+#   {
+#     "IP": "192.168.1.20",
+#     "MAC": "08-11-96-15-77-5C",
+#     "name": "",
+#     "onUSB": false,
+#     "source": "PrimaryAP"
+#   },
+#   {
+#     "IP": "192.168.1.60",
+#     "MAC": "D4-25-8B-94-D9-78",
+#     "name": "DESKTOP-O7JE7JE",
+#     "onUSB": false,
+#     "source": "PrimaryAP"
+#   },
+#   {}
+# ]
