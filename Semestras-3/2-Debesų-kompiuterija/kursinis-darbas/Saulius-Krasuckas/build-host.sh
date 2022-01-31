cea7595848aeecc22c5517267890c6c748f0046f
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-01-31 16:41:06 +0200

    build-host.sh: Pagrindiniai hosto kintamieji

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/build-host.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/build-host.sh
@@ -5,4 +5,8 @@
 # Kopijuosiu skripto dalis iš praėjusio semestro IiSA laboratorinių darbų:
 # https://github.com/VGTU-ELF/TETfm-20/blob/main/Semestras-2/1-Informacijos-ir-sistem%C5%B3-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
 
+BASE_DIR=$(builtin cd $(dirname $0); pwd)                   # Darbinė direktorija ten, kur skriptas
+LOG_FILE=${BASE_DIR}/$(basename ${0%.sh}).log               # Log failo vardas pagal skripto vardą (tik pakeičiu plėtinį)
+LOG_UART=${LOG_FILE%.log}-serial.log                        # Log failas VMų Serial/UART konsolei
+
 echo "Startuojama infrastruktūra"
