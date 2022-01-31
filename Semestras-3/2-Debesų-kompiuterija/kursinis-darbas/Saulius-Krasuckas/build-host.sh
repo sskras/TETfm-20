61cb70aa4e02e9296ba50aeb04d053f3aff71466
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-01-31 16:17:00 +0200

    build-host.sh: Pradedu skriptą

---
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/build-host.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# 2022-01-31 saukrs: Pradedu kursinį darbą.
+#
+# Kopijuosiu skripto dalis iš praėjusio semestro IiSA laboratorinių darbų:
+# https://github.com/VGTU-ELF/TETfm-20/blob/main/Semestras-2/1-Informacijos-ir-sistem%C5%B3-apsauga/laboratoriniai-darbai/Saulius-Krasuckas/0LD-infra.sh
