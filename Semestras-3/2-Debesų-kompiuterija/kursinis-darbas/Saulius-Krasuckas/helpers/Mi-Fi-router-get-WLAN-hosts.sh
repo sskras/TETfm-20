8629ad9ab1fb0a01f8dcca39c4ad874c369d68d9
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-01-31 14:49:39 +0200

    helpers/Mi-Fi-router-get-WLAN-hosts.sh: Grupuoju stotelių MAC ir IP adresus

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
@@ -3,7 +3,8 @@ curl 'http://192.168.1.1/api/model.json?internalapi=1&x=81601' \
   --compressed \
   --insecure \
   | sed 's/\r//' \
-  | /mingw64/bin/jq '.router.clientList'
+  | /mingw64/bin/jq '.router.clientList' \
+  | awk 'BEGIN {FS="[ \":]+"} /IP.:/ {IP=$3} /MAC/ {MAC=$3; print MAC"    "IP}'
 
 # jq iškerpta tokį gabalėlį:
 #
@@ -24,3 +25,8 @@ curl 'http://192.168.1.1/api/model.json?internalapi=1&x=81601' \
 #   },
 #   {}
 # ]
+
+# awk sugrupuoja MAC ir IP va tokiu pavidalu:
+#
+# 08-11-96-15-77-5C    192.168.1.20
+# D4-25-8B-94-D9-78    192.168.1.60
