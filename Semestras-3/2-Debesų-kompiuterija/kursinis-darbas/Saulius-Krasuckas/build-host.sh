2237a90cbcda5707fa2148bfd5f66870c6ebb8a8
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-01-31 16:56:19 +0200

    build-host.sh: Pradedu gražinti kintamųjų vardus.

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/build-host.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/build-host.sh
@@ -7,7 +7,7 @@
 
 BASE_DIR=$(builtin cd $(dirname $0); pwd)                   # Darbinė direktorija ten, kur skriptas
 LOG_FILE=${BASE_DIR}/$(basename ${0%.sh}).log               # Log failo vardas pagal skripto vardą (tik pakeičiu plėtinį)
-LOG_UART=${LOG_FILE%.log}-serial.log                        # Log failas VMų Serial/UART konsolei
+UART_LOG=${LOG_FILE%.log}-serial.log                        # Log failas VMų Serial/UART konsolei
 
 
 exec > >(tee -i "${LOG_FILE}") 2>&1                         # Dubliuoju išvestį į logą
@@ -43,7 +43,7 @@ VBoxManage_setup_serial_console () {
     screen -L telnet 127.0.0.1 23001
 
     # Išsaugome Serial konsolės logą Plain-text formatu:
-    cat screenlog.0 | ansifilter > ${LOG_UART}
+    cat screenlog.0 | ansifilter > ${UART_LOG}
 }
 
 echo "$(basename $0): Startuojama infrastruktūra"
