01a144f3e2d18c8a8ce7cb9f3b28c40ee2d3aae4
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-02-01 10:41:31 +0200

    build-host*.*: Atstatau poros kintamųjų išvestį atskirose eilutėse

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/build-host.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/build-host.sh
@@ -71,9 +71,11 @@ echo "$(basename $0): Startuojama infrastruktūra"
                                                                cd ${BASE_DIR}/VMs
     echo -e "\n- Host OS atvaizdžio parsiuntimas:\n"         ; curl -LC - -o ${VDI_ZIP} ${VDI_URL}
     echo -e "\n- Host OS atvaizdžio išspaudimas:\n"          ; time bsdtar -xvkf ${VDI_ZIP}
-                                                               bsdtar -tvf ${VDI_ZIP} | awk '/vdi$/ {$1=$2=$3=$4=$5=$6=$7=$8=""; print}' | read VDI_FILE; echo -e "\n  ${VDI_FILE}"
+                                                               bsdtar -tvf ${VDI_ZIP} | awk '/vdi$/ {$1=$2=$3=$4=$5=$6=$7=$8=""; print}' | read VDI_FILE
+    echo -e "\n- Host OS atvaizdis:\n"                       ; echo "${VDI_FILE}"
     echo -e "\n- Host OS atvaizdžio informacija:\n"          ; VBoxManage showmediuminfo disk "${VDI_FILE}"
-                                                               VBoxManage showmediuminfo disk "${VDI_FILE}" | awk '/^UUID/ {print $2}' | read VDI_UUID; echo -e "\n  ${VDI_UUID}"
+                                                               VBoxManage showmediuminfo disk "${VDI_FILE}" | awk '/^UUID/ {print $2}' | read VDI_UUID
+    echo -e "\n- Host OS atvaizdžio ID:\n"                   ; echo "${VDI_UUID}"
                                                                cd - > /dev/null
 
     echo -e "\n- Pradinės VM:\n"                             ; VBoxManage list vms
