6211ee257a39403d3a594218db97aaa13a258653
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-02-01 13:41:49 +0200

    helpers/Mi-Fi-router-get-WLAN-hosts.sh: Prided _shebang_.

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 curl 'http://192.168.1.1/api/model.json?internalapi=1&x=81601' \
   -H 'Cookie: p=password; sessionId=00000001-yTg32I35Tsygf43pjCurMKR8O3P9bbf' \
   --compressed \
