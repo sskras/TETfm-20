4b29078303875fa87a7f4ed5bbdd8aa942b78a14
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-02-01 13:54:48 +0200

    helpers/Mi-Fi-router-get-WLAN-hosts.sh: Nauja Web-Mgmt sesija.

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/helpers/Mi-Fi-router-get-WLAN-hosts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 curl 'http://192.168.1.1/api/model.json?internalapi=1&x=81601' \
-  -H 'Cookie: p=password; sessionId=00000001-yTg32I35Tsygf43pjCurMKR8O3P9bbf' \
+  -H 'Cookie: p=password; sessionId=00000001-BNUuYVXENzn5cjdY4Q00CBLCj7MKeTU' \
   --compressed \
   --insecure \
   | sed 's/\r//' \
