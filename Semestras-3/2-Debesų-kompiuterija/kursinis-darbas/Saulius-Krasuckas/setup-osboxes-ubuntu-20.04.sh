8f927e7220b775513f6783f2432927cf89e8c01d
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-02-04 21:33:12 +0200

    setup-osboxes-ubuntu-20.04.sh: Usage: sudo command Runs command with administrator privileges. be slaptažodžio

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-osboxes-ubuntu-20.04.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-osboxes-ubuntu-20.04.sh
@@ -3,4 +3,6 @@
 IP=$1
 
 sshpass -p osboxes.org ssh-copy-id -o StrictHostKeyChecking=no osboxes@${IP}
+ssh osboxes@${IP} "sudo -p '' -S bash -c 'echo osboxes ALL=\(ALL:ALL\) NOPASSWD: ALL | tee /etc/sudoers.d/osboxes'" < <(echo osboxes.org)
+
 ssh osboxes@${IP}
