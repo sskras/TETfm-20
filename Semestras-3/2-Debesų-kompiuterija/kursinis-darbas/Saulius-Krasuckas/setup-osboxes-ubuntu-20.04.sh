33e40929346179c8460e84fbe1646d75c40a0274
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-02-05 11:16:20 +0200

    setup-osboxes-ubuntu-20.04.sh: pora TODO.
    Ir tuščios eilutės prieš klausimus (kad būtų lengviau juos pastebėt).

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-osboxes-ubuntu-20.04.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-osboxes-ubuntu-20.04.sh
@@ -13,8 +13,15 @@ ssh osboxes@${IP} "sudo timedatectl set-timezone Europe/Vilnius"
 ssh osboxes@${IP} "timedatectl"
 ssh osboxes@${IP} "sudo localectl set-locale LC_TIME=C.UTF-8"
 ssh osboxes@${IP} "localectl"
+echo
 echo -n "Upgreidinam? "; read
 ssh osboxes@${IP} "sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y"
+echo
 echo -n "Rebūtinam? "; read
 ssh osboxes@${IP} "sudo reboot"
+# TODO ping-wait for ${IP}
+echo
+echo -n "Jau VM gyva? "; read
+
+ssh osboxes@${IP} "echo install Docker here or in another script ?"
 ssh osboxes@${IP}
