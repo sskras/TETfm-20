776e40a4c26fa38f9783d905b8281ec5d1ac7924
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-02-05 20:27:11 +0200

    setup-osboxes-ubuntu-20.04.sh: Indentuoju receptą

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-osboxes-ubuntu-20.04.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-osboxes-ubuntu-20.04.sh
@@ -10,37 +10,37 @@ sshpass -p osboxes.org ssh-copy-id -o StrictHostKeyChecking=no osboxes@${IP}
 
 echo "Komandų receptas:"
 echo
-echo ".................................................................................................................."
+echo "......................................................................................................................"
 exec 8<>1; cat << \
 ------------------------------------------------------------------------------------------------------------------------ |
-sudo -p '' -S bash -c 'echo osboxes ALL=\\\(ALL:ALL\\\) NOPASSWD: ALL | tee /etc/sudoers.d/osboxes' <<< osboxes.org
+    sudo -p '' -S bash -c 'echo osboxes ALL=\\\(ALL:ALL\\\) NOPASSWD: ALL | tee /etc/sudoers.d/osboxes' <<< osboxes.org
 
-echo -e '127.0.2.1\\\t${TEMPLATE_HOSTNAME}' | sudo tee -a /etc/hosts
-sudo hostnamectl set-hostname ${TEMPLATE_HOSTNAME}
-hostnamectl
+    echo -e '127.0.2.1\\\t${TEMPLATE_HOSTNAME}' | sudo tee -a /etc/hosts
+    sudo hostnamectl set-hostname ${TEMPLATE_HOSTNAME}
+    hostnamectl
 
-sudo timedatectl set-timezone Europe/Vilnius
-timedatectl
+    sudo timedatectl set-timezone Europe/Vilnius
+    timedatectl
 
-sudo localectl set-locale LC_TIME=C.UTF-8
-localectl
-date
+    sudo localectl set-locale LC_TIME=C.UTF-8
+    localectl
+    date
 
-# Add nearer APT mirror?
+    # Add nearer APT mirror?
 
-# sudo sed -i.BACKUP-1 's|us.archive.ubuntu.com|ubuntu.mirror.vu.lt|' /etc/apt/sources.list
-# sudo sed -i.BACKUP-2 's|security.ubuntu.com|ubuntu.mirror.vu.lt|' /etc/apt/sources.list
+    # sudo sed -i.BACKUP-1 's|us.archive.ubuntu.com|ubuntu.mirror.vu.lt|' /etc/apt/sources.list
+    # sudo sed -i.BACKUP-2 's|security.ubuntu.com|ubuntu.mirror.vu.lt|' /etc/apt/sources.list
 
-echo -n "Upgreidinam:"
-sudo apt update
-sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
-sudo DEBIAN_FRONTEND=noninteractive apt install -y vim colordiff pv
+    echo -n "Upgreidinam:"
+    sudo apt update
+    sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
+    sudo DEBIAN_FRONTEND=noninteractive apt install -y vim colordiff pv
 
-echo -n "Rebūtinam:"
-nohup sudo -b bash -c 'sleep 2; reboot'
+    echo -n "Rebūtinam:"
+    nohup sudo -b bash -c 'sleep 2; reboot'
 ------------------------------------------------------------------------------------------------------------------------
 tee /dev/fd/8
-echo ".................................................................................................................."
+echo "......................................................................................................................"
 echo
 
 while read -u 8 REMOTE_CMD; do
