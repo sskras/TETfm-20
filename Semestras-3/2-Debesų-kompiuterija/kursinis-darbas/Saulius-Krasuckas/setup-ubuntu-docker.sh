9ab257698bd097607264d290c5e262826f537e0d
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-02-06 12:05:30 +0200

    setup-ubuntu-docker.sh: get.docker.com eiga

---
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-ubuntu-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+exit
+
+# From docker-install script, commit: 93d2499759296ac1f9c510605fef85052a2c32be
++ sudo -E sh -c apt-get update -qq >/dev/null
++ sudo -E sh -c DEBIAN_FRONTEND=noninteractive apt-get install -y -qq apt-transport-https ca-certificates curl >/dev/null
++ sudo -E sh -c curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | gpg --dearmor --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
+gpg: WARNING: unsafe ownership on homedir '/home/osboxes/.gnupg'
++ sudo -E sh -c echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable" > /etc/apt/sources.list.d/docker.list
++ sudo -E sh -c apt-get update -qq >/dev/null
++ sudo -E sh -c DEBIAN_FRONTEND=noninteractive apt-get install -y -qq --no-install-recommends  docker-ce-cli docker-scan-plugin docker-ce >/dev/null
++ version_gte 20.10
++ [ -z  ]
++ return 0
++ sudo -E sh -c DEBIAN_FRONTEND=noninteractive apt-get install -y -qq docker-ce-rootless-extras >/dev/null
++ sudo -E sh -c docker version
