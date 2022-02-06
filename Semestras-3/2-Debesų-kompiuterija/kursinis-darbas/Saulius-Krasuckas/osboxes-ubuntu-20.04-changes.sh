6c1fab0d9ff31154e68807a653fafdd38079e23c
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-02-06 21:01:25 +0200

    setup-osboxes-*.sh: Nuindentuoju

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/osboxes-ubuntu-20.04-changes.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/osboxes-ubuntu-20.04-changes.sh
@@ -3,45 +3,45 @@ TEMPLATE_HOSTNAME="sksw-TODO"
 echo
 echo - sudo be promptų:
 echo
-    sudo -p '' -S bash -c "echo osboxes ALL=\(ALL:ALL\) NOPASSWD: ALL | tee /etc/sudoers.d/osboxes" <<< osboxes.org
+sudo -p '' -S bash -c "echo osboxes ALL=\(ALL:ALL\) NOPASSWD: ALL | tee /etc/sudoers.d/osboxes" <<< osboxes.org
 
 echo
 echo - sudo be lago:
 echo
-    echo -e "127.0.2.1\t${TEMPLATE_HOSTNAME}" | sudo tee -a /etc/hosts
+echo -e "127.0.2.1\t${TEMPLATE_HOSTNAME}" | sudo tee -a /etc/hosts
 echo
-    sudo hostnamectl set-hostname ${TEMPLATE_HOSTNAME}
-    hostnamectl
+sudo hostnamectl set-hostname ${TEMPLATE_HOSTNAME}
+hostnamectl
 
 echo
 echo - Laiko juosta:
 echo
-    sudo timedatectl set-timezone Europe/Vilnius
-    timedatectl
+sudo timedatectl set-timezone Europe/Vilnius
+timedatectl
 
 echo
 echo - Laiko formatas be AM/PM:
 echo
-    sudo localectl set-locale LC_TIME=C.UTF-8
-    localectl
+sudo localectl set-locale LC_TIME=C.UTF-8
+localectl
 echo
-    date
+date
 
 echo
 echo - APT Mirror perjungimas:
 echo
-    # Add nearer APT mirror?
+# Add nearer APT mirror?
 
-    # sudo sed -i.BACKUP-1 's|us.archive.ubuntu.com|ubuntu.mirror.vu.lt|' /etc/apt/sources.list
-    # sudo sed -i.BACKUP-2 's|security.ubuntu.com|ubuntu.mirror.vu.lt|' /etc/apt/sources.list
+# sudo sed -i.BACKUP-1 's|us.archive.ubuntu.com|ubuntu.mirror.vu.lt|' /etc/apt/sources.list
+# sudo sed -i.BACKUP-2 's|security.ubuntu.com|ubuntu.mirror.vu.lt|' /etc/apt/sources.list
 
 echo
 echo - Upgreidinam:
-    sudo apt update
-    sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
-    sudo DEBIAN_FRONTEND=noninteractive apt install -y vim colordiff pv curl iotop htop sysstat
+sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt install -y vim colordiff pv curl iotop htop sysstat
 
 echo
 echo - Rebūtinam:
 echo
-    nohup sudo -b bash -c "sleep 2; reboot"
+nohup sudo -b bash -c "sleep 2; reboot"
