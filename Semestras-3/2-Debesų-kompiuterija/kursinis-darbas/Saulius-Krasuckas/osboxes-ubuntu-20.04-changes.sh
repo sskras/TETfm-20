10ce02ab1c250d388151ad75547b7e6240c5fa05
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-02-06 14:31:37 +0200

    setup-osboxes*.sh,*-changes.sh: Įjungiu Hostname keitimą.
    Taip pat tikslinu/taisau žingsnių vardų formatavimą

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/osboxes-ubuntu-20.04-changes.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/osboxes-ubuntu-20.04-changes.sh
@@ -3,7 +3,7 @@ TEMPLATE_HOSTNAME="sksw-TODO"
     sudo -p '' -S bash -c "echo osboxes ALL=\(ALL:ALL\) NOPASSWD: ALL | tee /etc/sudoers.d/osboxes" <<< osboxes.org
 
     echo -e "127.0.2.1\t${TEMPLATE_HOSTNAME}" | sudo tee -a /etc/hosts
-   #sudo hostnamectl set-hostname ${TEMPLATE_HOSTNAME}
+    sudo hostnamectl set-hostname ${TEMPLATE_HOSTNAME}
     hostnamectl
 
     sudo timedatectl set-timezone Europe/Vilnius
@@ -18,10 +18,12 @@ TEMPLATE_HOSTNAME="sksw-TODO"
     # sudo sed -i.BACKUP-1 's|us.archive.ubuntu.com|ubuntu.mirror.vu.lt|' /etc/apt/sources.list
     # sudo sed -i.BACKUP-2 's|security.ubuntu.com|ubuntu.mirror.vu.lt|' /etc/apt/sources.list
 
-    echo -n "Upgreidinam:"
+echo
+echo - Upgreidinam:
     sudo apt update
     sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
     sudo DEBIAN_FRONTEND=noninteractive apt install -y vim colordiff pv curl iotop htop sysstat
 
-    echo -n "Rebūtinam:"
+echo
+echo - Rebūtinam:
     nohup sudo -b bash -c "sleep 2; reboot"
--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-osboxes-ubuntu-20.04.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-osboxes-ubuntu-20.04.sh
@@ -18,16 +18,20 @@ cat osboxes-ubuntu-20.04-changes.sh | ssh osboxes@${IP}
 
 echo
 echo - Laukiam IP išjungimo:
+echo
 ping -c 4 -W 1 ${IP} | sed "1d; / ms$/! q"
 
 echo
 echo - Laukiam IP įjungimo:
+echo
 ping ${IP} | sed "/ ms$/ q"
 
 echo
 echo - Docker diegimas ...
+echo
 cat setup-ubuntu-docker.sh | ssh osboxes@${IP}
 
 echo
 echo - Paskutinis patikrinimas, uptime:
+echo
 ssh osboxes@${IP} "uptime"
