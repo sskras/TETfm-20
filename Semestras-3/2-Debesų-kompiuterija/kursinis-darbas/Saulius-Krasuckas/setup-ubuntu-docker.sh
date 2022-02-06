19a6a9a64b6ecb3eed46ee476c2fefaf0e1dcc71
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-02-06 12:26:07 +0200

    setup-ubuntu-docker.sh: Perkeliu naujus paketus

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-ubuntu-docker.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-ubuntu-docker.sh
@@ -5,13 +5,13 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y apt-transport-https ca-certif
 curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | sudo gpg --dearmor --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive apt install -y docker-ce-cli docker-scan-plugin docker-ce
+sudo DEBIAN_FRONTEND=noninteractive apt install -y docker-ce-rootless-extras
 
 exit
 
 # From docker-install script, commit: 93d2499759296ac1f9c510605fef85052a2c32be
-+ sudo -E sh -c DEBIAN_FRONTEND=noninteractive apt-get install -y -qq --no-install-recommends  docker-ce-cli docker-scan-plugin docker-ce >/dev/null
 + version_gte 20.10
 + [ -z  ]
 + return 0
-+ sudo -E sh -c DEBIAN_FRONTEND=noninteractive apt-get install -y -qq docker-ce-rootless-extras >/dev/null
 + sudo -E sh -c docker version
