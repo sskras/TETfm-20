e79dda2d660fc9461529f5ac9173bdb14d601670
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-02-08 17:26:29 +0200

    setup-ubuntu-hostnames.sh: Registruoju OAM-IP

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-ubuntu-hostnames.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-ubuntu-hostnames.sh
@@ -1,6 +1,7 @@
 shopt -s lastpipe
 
 APP_IF_NUM=2
+OAM_IF_NUMB=3
 HOSTNAME=$1
 
 get_IF_by_numb () {
@@ -15,3 +16,8 @@ get_IF_by_numb 2 | read APP_IF
 get_IP_by_name ${APP_IF} | read APP_IP
 
 echo -e "${APP_IP}\t${HOSTNAME}" | sudo tee -a /etc/hosts
+
+get_IF_by_numb ${OAM_IF_NUMB} | read OAM_IF_NAME
+get_IP_by_name ${OAM_IF_NAME} | read OAM_IP
+
+echo -e "${OAM_IP}\t${HOSTNAME}-oam" | sudo tee -a /etc/hosts
