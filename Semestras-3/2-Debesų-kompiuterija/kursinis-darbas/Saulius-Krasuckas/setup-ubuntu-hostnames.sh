2a958b3c6023eb34c8b497ac22b604ba5a392f26
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-02-08 17:37:22 +0200

    setup-ubuntu-hostnames.sh: Patikslinu kintamojo vardą

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-ubuntu-hostnames.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-ubuntu-hostnames.sh
@@ -2,7 +2,7 @@ shopt -s lastpipe
 
 APP_IF_NUMB=2
 OAM_IF_NUMB=3
-HOSTNAME=$1
+NEW_HOSTNAME=$1
 
 get_IF_by_numb () {
     ip a | awk 'BEGIN {FS="[ :]+"} /^[0-9]/ {NUM++; if (NUM=='$1') print $2}'
@@ -15,9 +15,9 @@ get_IP_by_name () {
 get_IF_by_numb ${APP_IF_NUMB} | read APP_IF_NAME
 get_IP_by_name ${APP_IF_NAME} | read APP_IP
 
-echo -e "${APP_IP}\t${HOSTNAME}" | sudo tee -a /etc/hosts
+echo -e "${APP_IP}\t${NEW_HOSTNAME}" | sudo tee -a /etc/hosts
 
 get_IF_by_numb ${OAM_IF_NUMB} | read OAM_IF_NAME
 get_IP_by_name ${OAM_IF_NAME} | read OAM_IP
 
-echo -e "${OAM_IP}\t${HOSTNAME}-oam" | sudo tee -a /etc/hosts
+echo -e "${OAM_IP}\t${NEW_HOSTNAME}-oam" | sudo tee -a /etc/hosts
