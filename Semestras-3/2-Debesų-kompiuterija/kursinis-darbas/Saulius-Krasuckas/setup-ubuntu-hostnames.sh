3b8257246378e9bf8f767edce6d6b9481776c8c9
Saulius Krasuckas <saulius.krasuckas@stud.vilniustech.lt>
2022-02-08 17:24:30 +0200

    setup-ubuntu-hostnames.sh: Registruoju App-IP

--- Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-ubuntu-hostnames.sh
+++ Semestras-3/2-Debesų-kompiuterija/kursinis-darbas/Saulius-Krasuckas/setup-ubuntu-hostnames.sh
@@ -1,3 +1,17 @@
 shopt -s lastpipe
-ip a | awk 'BEGIN {FS="[ :]+"} /^[0-9]/ {NUM++; if (NUM==2) print $2}' | read APP_IF
-ip a show dev ${APP_IF} | awk 'BEGIN {FS="[ /]+"} /inet / {print $3}' | read APP_IP
+
+APP_IF_NUM=2
+HOSTNAME=$1
+
+get_IF_by_numb () {
+    ip a | awk 'BEGIN {FS="[ :]+"} /^[0-9]/ {NUM++; if (NUM=='$1') print $2}'
+}
+
+get_IP_by_name () {
+    ip a show dev $1 | awk 'BEGIN {FS="[ /]+"} /inet / {print $3}'
+}
+
+get_IF_by_numb 2 | read APP_IF
+get_IP_by_name ${APP_IF} | read APP_IP
+
+echo -e "${APP_IP}\t${HOSTNAME}" | sudo tee -a /etc/hosts
